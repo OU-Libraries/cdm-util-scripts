f567cc0a9d65b9e29a34981b1e99a3c52e5c5087
Nick <versteeg@ohio.edu>
2020-10-27 13:24:44 -0400

    Polish ftp2catcher style

--- ftp2catcher.py
+++ ftp2catcher.py
@@ -2,6 +2,7 @@ import requests
 
 import json
 import argparse
+from itertools import count
 
 from typing import List
 
@@ -29,7 +30,10 @@ def get_ftp_manifest(url: str, session: requests.Session) -> dict:
 
 def get_ftp_manifest_transcript_urls(manifest: dict, label: str) -> List[str]:
     canvases = manifest['sequences'][0]['canvases']
-    return [seeAlso['@id'] for canvas in canvases for seeAlso in canvas['seeAlso'] if seeAlso['label'] == label]
+    return [seeAlso['@id']
+            for canvas in canvases
+            for seeAlso in canvas['seeAlso']
+            if seeAlso['label'] == label]
 
 
 def get_ftp_transcript(url: str, session: requests.Session) -> str:
@@ -54,7 +58,7 @@ def main():
     parser.add_argument('source_nick',
                         metavar='source_nick',
                         type=str,
-                        help="CONTENTdm field nickname for FromThePage's dc:source")
+                        help="CONTENTdm field nickname for FromThePage's IIIF dc:source metadata field")
     parser.add_argument('transcript_nick',
                         metavar='transcript_nick',
                         type=str,
@@ -66,35 +70,50 @@ def main():
     parser.add_argument('output_file',
                         metavar='output_file',
                         type=str,
-                        help="Path to cdm-catcher JSON file")
+                        help="Path to write the cdm-catcher JSON file")
     args = parser.parse_args()
+
+    transcript_type_label = 'Verbatim Plaintext'
 
     with open(args.manifests_file, mode='r') as fp:
         manifest_urls = [line.strip() for line in fp.readlines()]
 
-    alias = args.collection_alias
-    source_nick = args.source_nick
-    transcript_nick = args.transcript_nick
-
     catcher_fields = []
     with requests.Session() as session:
         for manifest_url in manifest_urls:
+            print(f"Requesting {manifest_url!r}...")
             manifest = get_ftp_manifest(manifest_url, session)
-            source = [field['value'] for field in manifest['metadata'] if field['label'] == 'dc:source'][0]
-            object_pointers = find_objects(alias, source_nick, source, session)
-            if len(object_pointers) != 1:
-                raise ValueError(f"No unique object found for {source!r} in {source_nick!r}")
-            page_pointers = get_documentpdf_page_pointers(alias, object_pointers[0], session)
-            transcript_urls = get_ftp_manifest_transcript_urls(manifest,
-                                                               label='Verbatim Plaintext')
+            source = [field['value']
+                      for field in manifest['metadata']
+                      if field['label'] == 'dc:source'][0]
+            print(f"Searching {args.collection_alias!r} field {args.source_nick!r} for {source!r}...")
+            cdm_object_pointers = find_objects(alias=args.collection_alias,
+                                               field_nick=args.source_nick,
+                                               value=source,
+                                               session=session)
+            if len(cdm_object_pointers) != 1:
+                raise ValueError(f"No unique object found for {source!r} in {args.source_nick!r}")
+            page_pointers = get_documentpdf_page_pointers(alias=args.collection_alias,
+                                                          dmrecord=cdm_object_pointers[0],
+                                                          session=session)
+            transcript_urls = get_ftp_manifest_transcript_urls(manifest=manifest,
+                                                               label=transcript_type_label)
             if len(page_pointers) != len(transcript_urls):
                 raise ValueError(f"CONTENTdm/FromThePage object length mismatch")
+            print(f"Requesting {len(transcript_urls)} {transcript_type_label!r} page transcripts: ", end='')
+            n = count(1)
             for dmrecord, transcript_url in zip(page_pointers, transcript_urls):
+                print(f"{next(n)} ", end='')
                 transcript_text = get_ftp_transcript(transcript_url, session)
-                catcher_fields.append({'dmrecord': dmrecord,
-                                       transcript_nick: transcript_text})
+                catcher_fields.append({
+                    'dmrecord': dmrecord,
+                    args.transcript_nick: transcript_text
+                })
+            print(end='\n')
+    print("Writing JSON file...")
     with open(args.output_file, mode='w') as fp:
         json.dump(catcher_fields, fp, indent=2)
+    print("Done")
 
 
 if __name__ == '__main__':
