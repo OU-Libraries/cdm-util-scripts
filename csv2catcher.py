7a2f664a9f212814caa715bbb070440ff5e2a75b
Nick <versteeg@ohio.edu>
2020-11-03 12:26:26 -0500

    Refactor csv2catcher

--- csv2catcher.py
+++ csv2catcher.py
@@ -3,8 +3,8 @@ import requests
 import json
 import csv
 import argparse
-from collections import defaultdict
 from dataclasses import dataclass
+from itertools import count
 
 from typing import List, Optional
 
@@ -33,14 +33,18 @@ def get_cdm_collection(repo_url: str,
                                      identifier=record[id_field_nick],
                                      is_cpd=record['find'].endswith('.cpd'))
                            for record in dmQuery['records']]
-    print(f"Found {total} records in {alias}")
+    cpd_total = sum([1 if o.is_cpd else 0 for o in cdm_collection])
+    print(f"Found {total} records in {alias}, {cpd_total} objects")
+    cpd_count = count(1)
     if page_data:
         for cdm_object in cdm_collection:
             if cdm_object.is_cpd:
-                cdm_object.page_pointers = get_cdm_page_pointers(repo_url,
-                                                                 alias,
-                                                                 cdm_object.pointer,
-                                                                 session)
+                n = next(cpd_count)
+                print(f"Fetching compound object structures: {n}/{cpd_total} {(n // cpd_total) * 100}%", end='\r', flush=True)
+                cdm_object.page_pointers = get_cdm_page_pointers(repo_url=repo_url,
+                                                                 alias=alias,
+                                                                 dmrecord=cdm_object.pointer,
+                                                                 session=session)
     return cdm_collection
 
 
@@ -50,7 +54,7 @@ def get_cdm_page_pointers(repo_url: str, alias: str, dmrecord: str, session: req
     dmGetCompoundObjectInfo = response.json()
     if 'code' in dmGetCompoundObjectInfo:
         raise ValueError(f"CONTENTdm error {dmGetCompoundObjectInfo['message']!r}")
-    print(f"Found {len(dmGetCompoundObjectInfo)} pages in {dmrecord!r} of type {dmGetCompoundObjectInfo['type']}", flush=True)
+    # print(f"Found {len(dmGetCompoundObjectInfo)} pages in {dmrecord!r} of type {dmGetCompoundObjectInfo['type']}", flush=True)
     return [page['pageptr'] for page in dmGetCompoundObjectInfo['page']]
 
 
@@ -85,20 +89,18 @@ def main():
     args = parser.parse_args()
 
     reconciliation_args = (args.key_nick, args.repository_url, args.collection_alias)
-    if not all(reconciliation_args) and any(reconciliation_args):
+    if any(reconciliation_args) and not all(reconciliation_args):
         raise ValueError("All of repository_url, collection_alias and key_nick must be specified for reconciliation")
 
     with open(args.column_mapping_file, mode='r') as fp:
         reader = csv.DictReader(fp)
         if reader.fieldnames != ['name', 'nick']:
-            raise KeyError("Column mapping file has non 'nick' and 'name' column titles")
+            raise ValueError("Column mapping file must have 'name' and 'nick' column titles in that order")
         name_nick_map = {row['name']: row['nick'] for row in reader}
 
-    catcher_records = []
     with open(args.ftp_all_table_data, mode='r') as fp:
-        reader = csv.DictReader(fp)
-        for row in reader:
-            catcher_records.append({nick: row[name] for name, nick in name_nick_map.items()})
+        catcher_records = [{nick: row[name] for name, nick in name_nick_map.items()}
+                           for row in csv.DictReader(fp)]
 
     if all(reconciliation_args):
         with requests.Session() as session:
@@ -114,22 +116,22 @@ def main():
                 if cdm_object.identifier in cdm_index:
                     raise ValueError(f"{cdm_object.identifier} not unique in collection")
                 cdm_index[cdm_object.identifier] = cdm_object
-
             if args.object_page:
-                catcher_records = [{'dmrecord': cdm_index[record[args.key_nick]].pointer, **record}
-                                   for record in catcher_records
-                                   if int(record['Page Position']) == args.object_page]
+                cdm_catcher_json = [{'dmrecord': cdm_index[record[args.key_nick]].pointer, **record}
+                                    for record in catcher_records
+                                    if int(record['Page Position']) == args.object_page]
             else:
+                cdm_catcher_json = []
                 for record in catcher_records:
                     cdm_object = cdm_index[record[args.key_nick]]
-                    if cdm_object.is_cpd:
-                        record['dmrecord'] = cdm_object.page_pointers[int(record['Page Position']) - 1]
-                    else:
-                        record['dmrecord'] = cdm_object.pointer
-                    del record[args.key_nick]
+                    cdm_catcher_json.append({
+                        'dmrecord': (cdm_object.page_pointers[int(record['Page Position']) - 1]
+                                     if cdm_object.is_cpd else cdm_object.pointer),
+                        **record
+                    })
 
     with open(args.output_file, mode='w') as fp:
-        json.dump(catcher_records, fp, indent=2)
+        json.dump(cdm_catcher_json, fp, indent=2)
 
 
 if __name__ == '__main__':
