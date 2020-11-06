a7d3092597751c3ffc7d36ae8e75013e0baa59a6
Nick <versteeg@ohio.edu>
2020-11-06 18:19:21 -0500

    Start functional refactor

--- csv2catcher.py
+++ csv2catcher.py
@@ -4,91 +4,148 @@ import json
 import csv
 import argparse
 from dataclasses import dataclass
+from collections import defaultdict
 from itertools import count
 
-from typing import List, Optional, Dict
+from ftp2catcher import get_cdm_page_pointers
+
+from typing import List, Optional, Dict, Sequence, Any
 
 
 @dataclass
 class CdmObject:
-    pointer: str
-    identifier: str
-    is_cpd: bool
+    pointer: Optional[str] = None
+    identifier: Optional[str] = None
+    fields: Optional[dict] = None
+    page_position: Optional[int] = None
+    is_cpd: Optional[bool] = None
     page_pointers: Optional[List[str]] = None
 
+    def __add__(self, other: object) -> 'CdmObject':
+        if not isinstance(other, CdmObject):
+            return NotImplemented
+        return CdmObject(pointer=self._combine(self.pointer, other.pointer),
+                         identifier=self._combine(self.identifier, other.identifier),
+                         fields=self._combine(self.fields, other.fields),
+                         page_position=self._combine(self.page_position, other.page_position),
+                         is_cpd=self._combine(self.is_cpd, other.is_cpd),
+                         page_pointers=self._combine(self.page_pointers, other.page_pointers))
 
-def get_cdm_collection(repo_url: str,
-                       alias: str,
-                       id_field_nick: str,
-                       session: requests.Session,
-                       page_data: bool = True) -> List[CdmObject]:
-    cdm_collection = []
+    @staticmethod
+    def _combine(a: object, b: object) -> object:
+        if a == b:
+            return a
+        if a and b:
+            raise ValueError("combination collision: {a!r} and {b!r}")
+        return a or b
+
+
+def get_cdm_collection_object_records(repo_url: str,
+                                      alias: str,
+                                      field_nicks: Sequence[str],
+                                      session: requests.Session) -> List[dict]:
+    cdm_records = []
     total = 1
-    while len(cdm_collection) < total:
-        response = session.get(f"{repo_url.rstrip('/')}/digital/bl/dmwebservices/index.php?q=dmQuery/{alias}/CISOSEARCHALL/dmrecord!{id_field_nick}/dmrecord/1024/0/1/0/0/0/0/1/json")
+    start = 1
+    maxrecs = 1024
+    while len(cdm_records) < total:
+        response = session.get(f"{repo_url.rstrip('/')}/digital/bl/dmwebservices/index.php?q=dmQuery/{alias}/CISOSEARCHALL/{'!'.join(field_nicks)}/pointer/{maxrecs}/{start}/1/0/0/0/0/1/json")
         response.raise_for_status()
         dmQuery = response.json()
         total = int(dmQuery['pager']['total'])
-        cdm_collection += [CdmObject(pointer=record['dmrecord'],
-                                     identifier=record[id_field_nick],
-                                     is_cpd=record['find'].endswith('.cpd'))
-                           for record in dmQuery['records']]
-    cpd_total = sum([1 if o.is_cpd else 0 for o in cdm_collection])
-    print(f"Found {total} records in {alias}, {cpd_total} objects")
-    cpd_count = count(1)
-    if page_data:
-        for cdm_object in cdm_collection:
-            if cdm_object.is_cpd:
-                n = next(cpd_count)
-                print(f"Fetching compound object structures: {n}/{cpd_total} {(n // cpd_total) * 100}%", end='\r', flush=True)
-                cdm_object.page_pointers = get_cdm_page_pointers(repo_url=repo_url,
-                                                                 alias=alias,
-                                                                 dmrecord=cdm_object.pointer,
-                                                                 session=session)
-        print(end='\n')
-    return cdm_collection
+        start += maxrecs
+        cdm_records += dmQuery['records']
+    return cdm_records
 
 
-def get_cdm_page_pointers(repo_url: str, alias: str, dmrecord: str, session: requests.Session) -> List[str]:
-    response = session.get(f"{repo_url.rstrip('/')}/digital/bl/dmwebservices/index.php?q=dmGetCompoundObjectInfo/{alias}/{dmrecord}/json")
-    response.raise_for_status()
-    dmGetCompoundObjectInfo = response.json()
-    if 'code' in dmGetCompoundObjectInfo:
-        raise ValueError(f"CONTENTdm error {dmGetCompoundObjectInfo['message']!r}")
-    # print(f"Found {len(dmGetCompoundObjectInfo)} pages in {dmrecord!r} of type {dmGetCompoundObjectInfo['type']}", flush=True)
-    return [page['pageptr'] for page in dmGetCompoundObjectInfo['page']]
+def build_identifier_to_object_pointer_index(cdm_records: List[dict], identifier_nick: str) -> Dict[str, CdmObject]:
+    identifier_index = dict()
+    for record in cdm_records:
+        identifier = record[identifier_nick]
+        if identifier in identifier_index:
+            raise KeyError(f"{identifier!r} duplicated")
+        identifier_index[identifier] = CdmObject(pointer=record['pointer'],
+                                                 identifier=identifier,
+                                                 is_cpd=record['find'].endswith('.cpd'))
+    return identifier_index
 
 
-def build_cdm_identifier_index(cdm_collection: List[CdmObject]) -> Dict[str, CdmObject]:
-    cdm_index = dict()
+def build_identifier_to_object_and_page_pointer_index(repo_url: str,
+                                                      alias: str,
+                                                      identifier_to_object_pointer_index: Dict[str, CdmObject],
+                                                      session: requests.Session,
+                                                      verbose: bool = True) -> Dict[str, CdmObject]:
+    if verbose:
+        total_cpd = sum(1 if o.is_cpd else 0 for o in identifier_to_object_pointer_index.values())
+        request_count = count(1)
+    object_and_page_pointer_index = dict()
+    for identifier, cdm_object in identifier_to_object_pointer_index.items():
+        if cdm_object.is_cpd:
+            if verbose:
+                n = next(request_count)
+                print(f"Requesting page pointers: {n}/{total_cpd} {(n // total_cpd) * 100}%",
+                      end='\r',
+                      flush=True)
+            page_pointers = get_cdm_page_pointers(
+                repo_url=repo_url,
+                alias=alias,
+                dmrecord=cdm_object.pointer,
+                session=session
+            )
+        else:
+            page_pointers = None
+        verbose and print(end='\n')
+        object_and_page_pointer_index[identifier] = cdm_object + CdmObject(page_pointers=page_pointers)
+    return object_and_page_pointer_index
+
+
+def build_cdm_collection_from_rows(rows: Sequence[dict],
+                                   column_mapping: dict,
+                                   identifier_nick: Optional[str] = None) -> List[CdmObject]:
+    return [cdm_object_from_row(row=row,
+                                column_mapping=column_mapping,
+                                identifier_nick=identifier_nick)
+            for row in rows]
+
+
+def cdm_object_from_row(row: dict,
+                        column_mapping: dict,
+                        identifier_nick: Optional[str] = None) -> CdmObject:
+    fields = {nick: row[name] for name, nick in column_mapping.items()}
+    return CdmObject(identifier=fields[identifier_nick] if identifier_nick else None,
+                     page_position=int(row['Page Position']),
+                     fields=fields)
+
+
+def build_identifier_to_object_index(cdm_collection: List[CdmObject]) -> Dict[str, List[CdmObject]]:
+    cdm_index = defaultdict(list)
     for cdm_object in cdm_collection:
-        if cdm_object.identifier in cdm_index:
-            raise ValueError(f"{cdm_object.identifier} not unique in collection")
-        cdm_index[cdm_object.identifier] = cdm_object
+        cdm_index[cdm_object.identifier].append(cdm_object)
+    for entry in cdm_index.values():
+        entry.sort(key=lambda cdm_object: cdm_object.page_position)
     return cdm_index
 
 
-def object_level_catcher_json(cdm_index: Dict[str, CdmObject],
-                              catcher_records: List[dict],
-                              key_nick: str,
-                              object_page: int) -> List[dict]:
-    return [{'dmrecord': cdm_index[record[key_nick]].pointer, **record}
-            for record in catcher_records
-            if int(record['Page Position']) == object_page]
+def reconcile_indexes_by_object(identifier_to_object_index: Dict[str, List[CdmObject]],
+                                identifier_to_object_pointer_index: Dict[str, CdmObject],
+                                object_page: int) -> List[CdmObject]:
+    reconciled = []
+    for identifier, cdm_object_pointer in identifier_to_object_pointer_index.items():
+        cdm_object_fields = identifier_to_object_index[identifier][object_page - 1]
+        reconciled.append(cdm_object_pointer + cdm_object_fields)
+    return reconciled
 
 
-def page_level_catcher_json(cdm_index: Dict[str, CdmObject],
-                            catcher_records: List[dict],
-                            key_nick: str) -> List[dict]:
-    cdm_catcher_json = []
-    for record in catcher_records:
-        cdm_object = cdm_index[record[key_nick]]
-        cdm_catcher_json.append({
-            'dmrecord': (cdm_object.page_pointers[int(record['Page Position']) - 1]
-                         if cdm_object.is_cpd else cdm_object.pointer),
-            **record
-        })
-    return cdm_catcher_json
+def reconcile_indexes_by_page(identifier_to_object_index: Dict[str, List[CdmObject]],
+                              identifier_to_object_and_page_pointer_index: Dict[str, CdmObject]) -> List[dict]:
+    reconciled = []
+    for identifier, cdm_object_pointer in identifier_to_object_and_page_pointer_index.items():
+        for pointer, cdm_object in zip(cdm_object_pointer.page_pointers, identifier_to_object_index[identifier]):
+            reconciled.append(cdm_object + CdmObject(pointer=pointer))
+
+
+def serialize_cdm_objects(cdm_objects: Sequence[CdmObject]) -> List[dict]:
+    return [{'dmrecord': cdm_object.pointer, **cdm_object.fields} for cdm_object in cdm_objects]
 
 
 def main():
@@ -111,7 +168,7 @@ def main():
                         type=str,
                         action='store',
                         help="CONTENTdm collection alias")
-    parser.add_argument('--key_nick',
+    parser.add_argument('--identifier_nick',
                         type=str,
                         action='store',
                         help="Column name to use as a search field to get dmrecord numbers")
@@ -121,7 +178,7 @@ def main():
                         help="Page number to be used as object metadata (e.g. 1)")
     args = parser.parse_args()
 
-    reconciliation_args = (args.key_nick, args.repository_url, args.collection_alias)
+    reconciliation_args = (args.identifier_nick, args.repository_url, args.collection_alias)
     if any(reconciliation_args) and not all(reconciliation_args):
         raise ValueError("All of repository_url, collection_alias and key_nick must be specified for reconciliation")
 
@@ -129,40 +186,50 @@ def main():
         reader = csv.DictReader(fp)
         if reader.fieldnames != ['name', 'nick']:
             raise ValueError("Column mapping file must have 'name' and 'nick' column titles in that order")
-        name_nick_map = {row['name']: row['nick'] for row in reader}
+        column_mapping = {row['name']: row['nick'] for row in reader}
 
     with open(args.ftp_all_table_data, mode='r') as fp:
-        catcher_records = [{nick: row[name] for name, nick in name_nick_map.items()}
-                           for row in csv.DictReader(fp)]
+        cdm_collection = build_cdm_collection_from_rows(
+            rows=csv.DictReader(fp),
+            column_mapping=column_mapping,
+            identifier_nick=args.identifier_nick
+        )
 
-    if not all(reconciliation_args):
-        cdm_catcher_json = catcher_records
-    else:
+    if all(reconciliation_args):
+        identifier_to_object_index = build_identifier_to_object_index(cdm_collection)
         with requests.Session() as session:
-            cdm_collection = get_cdm_collection(
+            cdm_records = get_cdm_collection_object_records(
                 repo_url=args.repository_url,
                 alias=args.collection_alias,
-                id_field_nick=args.key_nick,
-                session=session,
-                page_data=False if args.object_page else True
+                session=session
             )
-        cdm_index = build_cdm_identifier_index(cdm_collection)
+            identifier_to_object_pointer_index = build_identifier_to_object_pointer_index(
+                records=cdm_records,
+                identifier_nick=args.identifier_nick
+            )
+            if args.object_page:
+                reduced_identifier_to_object_pointer_index = {identifier: identifier_to_object_pointer_index[identifier]
+                                                              for identifier in identifier_to_object_index.keys()}
+                identifier_to_object_and_page_pointer_index = build_identifier_to_object_and_page_pointer_index(
+                    repo_url=args.repository_url,
+                    alias=args.collection_alias,
+                    identifier_to_object_pointer_index=reduced_identifier_to_object_pointer_index,
+                    session=session
+                )
         if args.object_page:
-            cdm_catcher_json = object_level_catcher_json(
-                cdm_index=cdm_index,
-                catcher_records=catcher_records,
-                key_nick=args.key_nick,
+            catcher_data = reconcile_indexes_by_object(
+                identifier_to_object_index=identifier_to_object_index,
+                identifier_to_object_pointer_index=identifier_to_object_pointer_index,
                 object_page=args.object_page
             )
         else:
-            cdm_catcher_json = page_level_catcher_json(
-                cdm_index=cdm_index,
-                catcher_records=catcher_records,
-                key_nick=args.key_nick
+            catcher_data = reconcile_indexes_by_page(
+                identifier_to_object_index=identifier_to_object_index,
+                identifier_to_object_and_page_pointer_index=identifier_to_object_and_page_pointer_index
             )
 
     with open(args.output_file, mode='w') as fp:
-        json.dump(cdm_catcher_json, fp, indent=2)
+        json.dump(serialize_cdm_objects(catcher_data), fp, indent=2)
 
 
 if __name__ == '__main__':
--- tests/test_csv2catcher.py
+++ tests/test_csv2catcher.py
@@ -33,30 +33,118 @@ def cdm_collection():
     ]
 
 
-def test_get_cdm_collection(session):
-    with cdm_vcr.use_cassette('test_get_cdm_collection.yml'):
-        cdm_collection = csv2catcher.get_cdm_collection(repo_url='https://media.library.ohio.edu',
-                                                        alias='p15808coll15',
-                                                        id_field_nick='identi',
-                                                        session=session)
-        assert cdm_collection
+@pytest.fixture()
+def catcher_records():
+    return [
+        {'identi': 'test01', 'feildnicka': 'a1', 'fieldnickb': 'b1', 'fieldnickc': 'c1'},
+        {'identi': 'test02', 'feildnicka': 'a2', 'fieldnickb': 'b2', 'fieldnickc': 'c2'},
+        {'identi': 'ryan_box09-tld_f11', 'feildnicka': 'a3', 'fieldnickb': 'b3', 'fieldnickc': 'c3'},
+    ]
 
 
-def test_get_cdm_page_pointers(session):
-    with cdm_vcr.use_cassette('test_get_cdm_page_pointers.yml'):
-        pointers = csv2catcher.get_cdm_page_pointers(repo_url='https://media.library.ohio.edu',
-                                                     alias='p15808coll15',
-                                                     dmrecord='969',
-                                                     session=session)
-        assert pointers
+@pytest.fixture()
+def cdm_records():
+    return [{'collection': '/p15808coll15',
+             'pointer': 314,
+             'filetype': 'cpd',
+             'parentobject': -1,
+             'identi': 'ryan_box50-tlb_f05',
+             'find': '315.cpd'},
+            {'collection': '/p15808coll15',
+             'pointer': 329,
+             'filetype': 'cpd',
+             'parentobject': -1,
+             'identi': 'ryan_box50-tlb_f10',
+             'find': '330.cpd'},
+            {'collection': '/p15808coll15',
+             'pointer': 254,
+             'filetype': 'cpd',
+             'parentobject': -1,
+             'identi': 'ryan_box50-tlb_f24',
+             'find': '255.cpd'},
+            {'collection': '/p15808coll15',
+             'pointer': 300,
+             'filetype': 'cpd',
+             'parentobject': -1,
+             'identi': 'ryan_box50-tlb_f27',
+             'find': '301.cpd'}]
 
 
-def test_build_cdm_identifier_index(cdm_collection):
-    cdm_index = csv2catcher.build_cdm_identifier_index(cdm_collection)
-    assert len(cdm_collection) == len(cdm_index)
-    for cdm_object in cdm_collection:
-        assert cdm_object.identifier in cdm_index
-
+def test_CdmObject__combine():
+    assert csv2catcher.CdmObject._combine(None, 'b') == 'b'
+    assert csv2catcher.CdmObject._combine('a', None) == 'a'
+    assert csv2catcher.CdmObject._combine(None, None) is None
+    assert csv2catcher.CdmObject._combine('a', 'a') == 'a'
     with pytest.raises(ValueError):
-        cdm_collection.append(cdm_collection[0])
-        csv2catcher.build_cdm_identifier_index(cdm_collection)
+        csv2catcher.CdmObject._combine('a', 'b')
+
+
+def test_CdmObject___add__():
+    a = csv2catcher.CdmObject(pointer='1',
+                              identifier='a')
+    b = csv2catcher.CdmObject(identifier='a',
+                              fields={'a': '1', 'b': '2'},
+                              page_position=1,
+                              is_cpd=True,
+                              page_pointers=['2', '3'])
+    c = a + b
+    assert c.pointer == a.pointer
+    assert c.identifier == a.identifier == b.identifier
+    assert c.fields == b.fields
+    assert c.page_position == b.page_position
+    assert c.is_cpd == b.is_cpd
+    assert c.page_pointers == b.page_pointers
+
+    d = csv2catcher.CdmObject(identifier='d')
+    with pytest.raises(ValueError):
+        a + d
+
+
+def test_get_cdm_collection_object_records(session):
+    with cdm_vcr.use_cassette('test_get_cdm_collection_object_records.yml'):
+        cdm_records = csv2catcher.get_cdm_collection_object_records(
+            repo_url='https://media.library.ohio.edu',
+            alias='p15808coll15',
+            field_nicks=['identi'],
+            session=session
+        )
+    assert cdm_records
+
+
+def test_build_identifier_to_object_pointer_index(cdm_records):
+    index = csv2catcher.build_identifier_to_object_pointer_index(
+        cdm_records=cdm_records,
+        identifier_nick='identi'
+    )
+    assert len(index) == len(cdm_records)
+
+
+def test_build_identifier_to_object_pointer_index_unique(cdm_records):
+    cdm_records[3]['identi'] = cdm_records[0]['identi']
+    with pytest.raises(KeyError):
+        csv2catcher.build_identifier_to_object_pointer_index(
+            cdm_records=cdm_records,
+            identifier_nick='identi'
+        )
+
+
+@pytest.mark.skip(reason="Duplicate identifiers in Ryan")
+def test_build_identifier_to_object_and_page_pointer_index(session):
+    with cdm_vcr.use_cassette('test_get_cdm_collection_object_records.yml'):
+        cdm_records = csv2catcher.get_cdm_collection_object_records(
+            repo_url='https://media.library.ohio.edu',
+            alias='p15808coll15',
+            field_nicks=['identi'],
+            session=session
+        )
+    identifier_to_object_pointer_index = csv2catcher.build_identifier_to_object_pointer_index(
+        cdm_records=cdm_records,
+        identifier_nick='identi'
+    )
+    with cdm_vcr.use_cassette('test_build_identifier_to_object_and_page_pointer_index.yml'):
+        identifier_to_object_and_page_pointer_index = csv2catcher.build_identifier_to_object_and_page_pointer_index(
+            repo_url='https://media.library.ohio.edu',
+            alias='p15808coll15',
+            identifier_to_object_pointer_index=identifier_to_object_pointer_index,
+            session=session)
+    assert identifier_to_object_and_page_pointer_index
