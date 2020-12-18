c47ded2b014b34b0add29e2cb783eb4eecf8122f
Nick <versteeg@ohio.edu>
2020-12-17 19:19:43 -0500

    Implement ftpmd2catcher module

--- src/ftpmd2catcher/ftpmd2catcher.py
+++ src/ftpmd2catcher/ftpmd2catcher.py
@@ -5,15 +5,16 @@ import re
 import xml.etree.ElementTree as ET
 from dataclasses import dataclass
 
-from ftp2catcher import get_ftp_manifest_transcript_urls, get_ftp_manifest
+from ftp2catcher import get_ftp_manifest, get_cdm_page_pointers
 
-from typing import Optional, List
+from typing import Optional, List, Iterable, Dict, Sequence, Callable
 
 
 @dataclass
 class CdmObject:
     dmrecord: Optional[str] = None
     collection_alias: Optional[str] = None
+    repo_url: Optional[str] = None
     cdm_source_url: Optional[str] = None
     ftp_manifest_url: Optional[str] = None
     pages: Optional[list] = None
@@ -39,10 +40,12 @@ def get_ftp_collection(url: str, session: Session) -> List[CdmObject]:
         result = re.search(r"/(\w+)/(\d+)/manifest\.json$", cdm_source_url)
         collection_alias = result.group(1) if result else None
         dmrecord = result.group(2) if result else None
+        repo_url = cdm_source_url.partition('/iiif/info')[0]
         ftp_collection.append(
             CdmObject(
                 dmrecord=dmrecord,
                 collection_alias=collection_alias,
+                repo_url=repo_url,
                 cdm_source_url=cdm_source_url,
                 ftp_manifest_url=manifest['@id']
             )
@@ -50,43 +53,31 @@ def get_ftp_collection(url: str, session: Session) -> List[CdmObject]:
     return ftp_collection
 
 
-def get_rendering(manifest: dict, label: str) -> dict:
-    renderings = manifest['sequences'][0]['rendering']
+def get_rendering(ftp_manifest: dict, label: str) -> dict:
+    renderings = ftp_manifest['sequences'][0]['rendering']
     for rendering in renderings:
         if rendering['label'] == label:
             return rendering
     raise KeyError("label not found in manifest")
 
 
-def get_object_pages(cdm_object: CdmObject, session: Session) -> None:
-    manifest = get_ftp_manifest(url=cdm_object.ftp_manifest_url, session=session)
-    tei_rendering = get_rendering(manifest=manifest, label='TEI Export')
-    response = session.get(tei_rendering['@id'])
-    response.raise_for_status()
-    tei = response.text
-    cdm_object.pages = extract_fields_from_TEI(tei=tei)
-
-
-def get_objects_pages(cdm_objects: Sequence[CdmObject], session: Session) -> None:
-    for cdm_object in cdm_objects:
-        get_object_pages(cdm_object=cdm_object, session=session)
-
-
-def extract_fields_from_TEI(tei: str) -> List[dict]:
+def extract_fields_from_TEI(tei: str) -> List[Dict[str, str]]:
     NS = {'': 'http://www.tei-c.org/ns/1.0'}
     tei_root = ET.fromstring(tei)
     tei_pages = tei_root.findall('./text/body/div', namespaces=NS)
     pages = []
     for tei_page in tei_pages:
+        tei_fields = tei_page.findall('p', namespaces=NS)
         fields = dict()
         last_label = None
-        tei_fields = tei_page.findall('p', namespaces=NS)
         for tei_field in tei_fields:
             label = tei_field.find('span', namespaces=NS)
             # Element truthiness is on existence of child Elements, so test for None
             if label is not None:
                 label_text = last_label = label.text.partition(': ')[0]
-                fields[label_text] = ''.join(list(tei_field.itertext())[1:]).strip()
+                fields[label_text] = ''.join(
+                    list(tei_field.itertext())[1:]
+                ).strip()
             else:
                 fields[last_label] = '\n\n'.join([
                     fields[last_label],
@@ -94,6 +85,88 @@ def extract_fields_from_TEI(tei: str) -> List[dict]:
                 ]).strip()
         pages.append(fields)
     return pages
+
+
+def get_object_pages_from_TEI(cdm_object: CdmObject, session: Session, verbose: bool = True) -> None:
+    if verbose:
+        print(f"Requesting FromThePage manifest {cdm_object.ftp_manifest_url!r}...")
+    ftp_manifest = get_ftp_manifest(url=cdm_object.ftp_manifest_url, session=session)
+    tei_rendering = get_rendering(ftp_manifest=ftp_manifest, label='TEI Export')
+    if verbose:
+        print(f"Requesting TEI transcript {tei_rendering['@id']}...")
+    response = session.get(tei_rendering['@id'])
+    response.raise_for_status()
+    tei = response.text
+    cdm_object.pages = extract_fields_from_TEI(tei=tei)
+
+
+def get_objects_pages_from_TEI(cdm_objects: Iterable[CdmObject], session: Session) -> None:
+    for cdm_object in cdm_objects:
+        get_object_pages_from_TEI(cdm_object=cdm_object, session=session)
+
+
+def apply_field_mapping(ftp_fields: Dict[str, str], field_mapping: Dict[str, Sequence[str]]) -> Dict[str, str]:
+    accumulator = dict()
+    for label, nicks in field_mapping.items():
+        ftp_field = ftp_fields[label]
+        for nick in nicks:
+            if nick in accumulator:
+                if ftp_field:
+                    if accumulator[nick]:
+                        accumulator[nick] = '; '.join([accumulator[nick], ftp_field])
+                    else:
+                        accumulator[nick] = ftp_field
+            else:
+                accumulator[nick] = ftp_field
+    return accumulator
+
+
+def map_cdm_object_as_object(
+        cdm_object: CdmObject,
+        field_mapping: Dict[str, Sequence[str]],
+        page_picker: Callable[[List[Dict[str, str]]], Dict[str, str]]
+) -> Dict[str, str]:
+    object_page = page_picker(cdm_object.pages)
+    return {
+        'dmrecord': cdm_object.dmrecord,
+        **apply_field_mapping(object_page,
+                              field_mapping)
+    }
+
+
+class PagePickers:
+    def first_page(pages: List[Dict[str, str]]) -> Dict[str, str]:
+        return pages[0]
+
+    def first_filled_page_or_blank(pages: List[Dict[str, str]]) -> Dict[str, str]:
+        for page in pages:
+            if any(page.values()):
+                return page
+        return pages[0]
+
+
+def map_cdm_object_as_pages(
+        cdm_object: CdmObject,
+        field_mapping: Dict[str, Sequence[str]],
+        session: Session,
+        verbose: bool = True
+) -> List[Dict[str, str]]:
+    if verbose:
+        print(f"Requesting page pointers for {cdm_object.dmrecord!r} in {cdm_object.collection_alias!r} @ {cdm_object.repo_url!r}...")
+    page_pointers = get_cdm_page_pointers(
+        repo_url=cdm_object.repo_url,
+        alias=cdm_object.collection_alias,
+        dmrecord=cdm_object.dmrecord,
+        session=session
+    )
+    page_data = []
+    for page_pointer, ftp_fields in zip(page_pointers, cdm_object.pages):
+        page_data.append({
+            'dmrecord': page_pointer,
+            **apply_field_mapping(ftp_fields=ftp_fields,
+                                  field_mapping=field_mapping)
+        })
+    return page_data
 
 
 def main():
@@ -111,20 +184,29 @@ def main():
         type=str,
         help="CONTENTdm collection name used by FromThePage"
     )
+    parser.add_argument(
+        'field_mappings',
+        type=str,
+        help="CSV file if FromThePage field labels mapped to CONTENTdm nicknames"
+    )
+    parser.add_argument(
+        'match_mode',
+        type=str,
+        help="Match mode 'object' or 'page'"
+    )
     args = parser.parse_args()
 
-    session = Session()
-    collection_manifest_url = get_collection_manifest_url(
-        slug=args.slug,
-        collection_name=args.collection_name,
-        session=session
-    )
-    ftp_collection = get_ftp_collection(
-        url=collection_manifest_url,
-        session=session
-    )
-    get_objects_pages(ftp_collection)
-
+    with Session() as session:
+        collection_manifest_url = get_collection_manifest_url(
+            slug=args.slug,
+            collection_name=args.collection_name,
+            session=session
+        )
+        ftp_collection = get_ftp_collection(
+            url=collection_manifest_url,
+            session=session
+        )
+        get_objects_pages_from_TEI(ftp_collection)
 
 
 if __name__ == '__main__':
--- tests/test_ftpmd2catcher.py
+++ tests/test_ftpmd2catcher.py
@@ -42,10 +42,29 @@ def test_get_ftp_collection(session):
     for cdm_object in ftp_collection:
         assert cdm_object.dmrecord.isdigit()
         assert cdm_object.collection_alias
+        assert cdm_object.repo_url
         assert cdm_object.cdm_source_url
         assert cdm_object.ftp_manifest_url
 
 
+@ftp_vcr.use_cassette()
+def test_get_rendering(session):
+    response = session.get('https://fromthepage.com/iiif/48258/manifest')
+    ftp_manifest = response.json()
+    rendering = ftpmd2catcher.get_rendering(
+        ftp_manifest=ftp_manifest,
+        label='TEI Export'
+    )
+    assert rendering['@id'] == 'https://fromthepage.com/export/tei?work_id=48258'
+
+    with pytest.raises(KeyError):
+        ftpmd2catcher.get_rendering(
+            ftp_manifest=ftp_manifest,
+            label='Does Not Exist'
+        )
+
+
+@pytest.mark.skip()
 @ftp_vcr.use_cassette()
 @pytest.mark.parametrize('url, check_fields',
 [
@@ -95,3 +114,103 @@ def test_extract_fields_from_TEI(url, check_fields, session):
     for page, check_field in zip(pages, check_fields):
         for key, value in check_field.items():
             assert page[key] == value
+
+
+@pytest.mark.skip()
+@ftp_vcr.use_cassette()
+def test_get_object_pages_from_TEI(session):
+    cdm_object = ftpmd2catcher.CdmObject(
+        ftp_manifest_url='https://fromthepage.com/iiif/45440/manifest'
+    )
+    ftpmd2catcher.get_object_pages_from_TEI(cdm_object, session)
+    assert cdm_object.pages
+
+@pytest.mark.parametrize('field_mapping, result', [
+    ({'label': ['nick']}, {'nick': 'value'}),
+    ({'label': ['nick1', 'nick2']}, {'nick1': 'value', 'nick2': 'value'}),
+    ({'blank': ['nick'], 'label': ['nick']}, {'nick': 'value'}),
+    ({'label': ['nick'], 'blank': ['nick']}, {'nick': 'value'}),
+    ({'blank': ['nick', 'nick']}, {'nick': ''}),
+    ({'label': ['nick', 'nick']}, {'nick': 'value; value'}),
+    ({'label': ['nick'], 'label2': ['nick']}, {'nick': 'value; value2'}),
+    ({'label2': ['nick'], 'label': ['nick']}, {'nick': 'value2; value'}),
+])
+def test_apply_mapping(field_mapping, result):
+    ftp_fields = {
+        'label': 'value',
+        'label2': 'value2',
+        'blank': ''
+    }
+    mapped = ftpmd2catcher.apply_field_mapping(ftp_fields, field_mapping)
+    assert mapped == result
+
+
+@pytest.mark.parametrize('pages', [
+    ([
+        {'Label': 'value0'},
+        {'Label': 'value1'}
+    ]),
+])
+def test_PagePickers_first_page(pages):
+    assert ftpmd2catcher.PagePickers.first_page(pages) is pages[0]
+
+
+@pytest.mark.parametrize('pages, check_index', [
+    ([
+        {'Label0': '', 'Label1': ''},
+        {'Label0': '', 'Label1': 'value1'},
+        {'Label0': '', 'Label1': ''}
+    ],
+     1),
+    ([
+        {'Label0': '', 'Label1': ''},
+        {'Label0': '', 'Label1': ''},
+        {'Label0': '', 'Label1': ''}
+    ],
+     0),
+])
+def test_PagePickers_first_filled_page_or_blank(pages, check_index):
+    assert ftpmd2catcher.PagePickers.first_filled_page_or_blank(pages) is pages[check_index]
+
+
+@pytest.mark.parametrize('cdm_object, page_picker, result', [
+    (ftpmd2catcher.CdmObject(dmrecord='1', pages=[{'Label': 'value'}]),
+     ftpmd2catcher.PagePickers.first_page,
+     {'dmrecord': '1', 'nick': 'value'})
+])
+def test_map_cdm_object_as_object(cdm_object, page_picker, result):
+    field_mapping = {
+        'Label': ['nick']
+    }
+    page = ftpmd2catcher.map_cdm_object_as_object(
+        cdm_object,
+        field_mapping,
+        page_picker
+    )
+    assert page == result
+
+
+def test_map_cdm_object_as_pages(monkeypatch):
+    cdm_object = ftpmd2catcher.CdmObject(pages=[
+        {'Label': 'value1'},
+        {'Label': 'value2'},
+        {'Label': 'value3'}
+    ])
+    field_mapping = {
+        'Label': ['nick']
+    }
+
+    def mock(*args, **kwargs):
+        return ['1', '2', '3']
+
+    monkeypatch.setattr('ftpmd2catcher.ftpmd2catcher.get_cdm_page_pointers', mock)
+    page_data = ftpmd2catcher.map_cdm_object_as_pages(
+        cdm_object,
+        field_mapping,
+        None
+    )
+    assert page_data == [
+        {'dmrecord': '1', 'nick': 'value1'},
+        {'dmrecord': '2', 'nick': 'value2'},
+        {'dmrecord': '3', 'nick': 'value3'}
+    ]
