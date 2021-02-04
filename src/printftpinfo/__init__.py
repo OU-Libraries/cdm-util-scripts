919cf6974214833eda3f35ed5044c4d8c18278e5
Nick <versteeg@ohio.edu>
2021-02-04 17:48:02 -0500

    Add printftpinfo module

---
+++ src/printftpinfo/__init__.py
@@ -0,0 +1,1 @@
+from printftpinfo.printftpinfo import *
---
+++ src/printftpinfo/printftpinfo.py
@@ -0,0 +1,24 @@
+from requests import Session
+
+import argparse
+
+from printcdminfo import print_as_table
+
+
+def get_slug_collections(slug: str, session: Session) -> dict:
+    response = session.get(f"https://fromthepage.com/iiif/collections/{slug}")
+    response.raise_for_status()
+    return response.json()
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Print FromThePage project information")
+    parser.add_argument(
+        'slug',
+        type=str,
+        help="FromThePage user slug"
+    )
+    args = parser.parse_args()
+    with Session() as session:
+        collections = get_slug_collections(slug=args.slug, session=session)
+    print_as_table(collections['collections'])
