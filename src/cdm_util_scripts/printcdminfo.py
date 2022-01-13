7af3c2bf631da5dd9707652f1197c7ec3b74ad9b
Nick <versteeg@ohio.edu>
2022-01-12 23:02:41 -0500

    Print tables with rich

--- src/cdm_util_scripts/printcdminfo.py
+++ src/cdm_util_scripts/printcdminfo.py
@@ -1,4 +1,6 @@
 import requests
+from rich.console import Console
+from rich.table import Table
 
 import argparse
 import csv
@@ -7,28 +9,18 @@ import sys
 from io import StringIO
 from typing import Dict, Sequence, Callable, Any
 
-from cdm_util_scripts.cdm_api import get_collection_field_info, get_collection_list, DmError
+from cdm_util_scripts import cdm_api
 
 
 def print_as_table(rows: Sequence[dict]) -> None:
-    widths = {name: col_max(rows, name) + 1 for name in rows[0].keys()}
-    print(ljust_row({key: key for key in rows[0].keys()}, widths, serializer=str))
-    print(ljust_row({key: '-' * len(key) for key in widths.keys()}, widths, serializer=str))
+    table = Table()
+    column_names = list(rows[0])
+    for name in column_names:
+        table.add_column(name)
     for row in rows:
-        print(ljust_row(row, widths))
-
-
-def col_max(rows: Sequence[dict], col_name: str) -> int:
-    return max(len(repr(col_name)),
-               len(repr(max(rows, key=lambda row: len(repr(row[col_name])))[col_name])))
-
-
-def ljust_row(row: dict,
-              widths: Dict[str, int],
-              default_width: int = 8,
-              serializer: Callable[[Any], str] = repr) -> str:
-    return ''.join(serializer(value).ljust(widths.get(key, default_width))
-                   for key, value in row.items())
+        table.add_row(*(repr(row[name]) for name in column_names))
+    console = Console()
+    console.print(table)
 
 
 def print_as_csv(dm_json):
@@ -36,55 +28,58 @@ def print_as_csv(dm_json):
         writer = csv.DictWriter(output, fieldnames=dm_json[0].keys())
         writer.writeheader()
         writer.writerows(dm_json)
-        print(output.getvalue(), end='')
+        print(output.getvalue(), end="")
 
 
 def print_as_json(dm_json):
-    print(json.dumps(dm_json))
+    print(json.dumps(dm_json, indent=2))
 
 
-output_formats = {
-    'json': print_as_json,
-    'csv': print_as_csv,
-    'table': print_as_table
-}
+output_formats = {"json": print_as_json, "csv": print_as_csv, "table": print_as_table}
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Print CONTENTdm collection information")
-    parser.add_argument('repository_url',
-                        type=str,
-                        help="CONTENTdm repository URL")
-    parser.add_argument('--alias',
-                        action='store',
-                        type=str,
-                        help="CONTENTdm collection alias")
-    parser.add_argument('--output',
-                        type=str,
-                        action='store',
-                        choices=list(output_formats.keys()),
-                        default='table',
-                        help=f"Output format, default table")
-    parser.add_argument('--columns',
-                        type=str,
-                        action='store',
-                        help="Specify columns to print in a comma separated string, as --columns name,nick")
+    parser = argparse.ArgumentParser(
+        description="Print CONTENTdm collection information",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("repository_url", type=str, help="CONTENTdm repository URL")
+    parser.add_argument(
+        "--alias", action="store", type=str, help="CONTENTdm collection alias"
+    )
+    parser.add_argument(
+        "--output",
+        type=str,
+        action="store",
+        choices=list(output_formats.keys()),
+        default="table",
+        help=f"Output format",
+    )
+    parser.add_argument(
+        "--columns",
+        type=str,
+        action="store",
+        help="Specify columns to print in a comma separated string, as --columns name,nick",
+    )
     args = parser.parse_args()
 
     try:
         if args.alias:
-            dm_result = get_collection_field_info(args.repository_url, args.alias, requests)
+            dm_result = cdm_api.get_collection_field_info(
+                args.repository_url, args.alias, requests
+            )
         else:
-            dm_result = get_collection_list(args.repository_url, requests)
-    except DmError as err:
+            dm_result = cdm_api.get_collection_list(args.repository_url, requests)
+    except cdm_api.DmError as err:
         print(err, file=sys.stderr)
         sys.exit(1)
 
     if args.columns:
-        columns = args.columns.split(',')
+        columns = args.columns.split(",")
         try:
-            dm_result = [{column: entry[column] for column in columns}
-                         for entry in dm_result]
+            dm_result = [
+                {column: entry[column] for column in columns} for entry in dm_result
+            ]
         except KeyError as err:
             print(f"{err.args[0]!r} is not a valid column name", file=sys.stderr)
             sys.exit(1)
@@ -94,5 +89,5 @@ def main():
     output_formats[args.output](dm_result)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- src/cdm_util_scripts/printftpinfo.py
+++ src/cdm_util_scripts/printftpinfo.py
@@ -1,19 +1,17 @@
-import argparse
-
 from requests import Session
+
+import argparse
 
 from cdm_util_scripts.printcdminfo import print_as_table
 from cdm_util_scripts.ftp_api import get_slug_collections
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Print FromThePage project information")
-    parser.add_argument(
-        'slug',
-        type=str,
-        help="FromThePage user slug"
+    parser = argparse.ArgumentParser(
+        description="Print FromThePage project information"
     )
+    parser.add_argument("slug", type=str, help="FromThePage user slug")
     args = parser.parse_args()
     with Session() as session:
         collections = get_slug_collections(slug=args.slug, session=session)
-    print_as_table(collections['collections'])
+    print_as_table(collections["collections"])
