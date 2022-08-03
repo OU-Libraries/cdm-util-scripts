75ed525f051d33f5055131a2e9414b288e1178a5
Nick <versteeg@ohio.edu>
2022-08-03 17:19:38 -0400

    Update printftpinfo

--- src/cdm_util_scripts/printftpinfo.py
+++ src/cdm_util_scripts/printftpinfo.py
@@ -1,17 +1,25 @@
-from requests import Session
+import requests
 
 import argparse
 
-from cdm_util_scripts.printcdminfo import print_as_table
+from cdm_util_scripts.printcdminfo import print_as_records
 from cdm_util_scripts.ftp_api import get_slug_collections
 
 
-def main():
+def main() -> int:
     parser = argparse.ArgumentParser(
         description="Print FromThePage project information"
     )
     parser.add_argument("slug", type=str, help="FromThePage user slug")
     args = parser.parse_args()
-    with Session() as session:
+
+    with requests.Session() as session:
         collections = get_slug_collections(slug=args.slug, session=session)
-    print_as_table(collections["collections"])
+
+    print_as_records(collections["collections"])
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
