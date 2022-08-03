c0a64e54b61f8bdaebfa40865773ccb83ea3237b
Nick <versteeg@ohio.edu>
2022-08-03 16:59:10 -0400

    Remove rich dependency from printcdminfo

--- src/cdm_util_scripts/printcdminfo.py
+++ src/cdm_util_scripts/printcdminfo.py
@@ -1,44 +1,37 @@
 import requests
-from rich.console import Console
-from rich.table import Table
 
 import argparse
 import csv
 import json
 import sys
-from io import StringIO
-from typing import Dict, Sequence, Callable, Any
+
+from typing import Dict, Iterable
 
 from cdm_util_scripts import cdm_api
 
 
-def print_as_table(rows: Sequence[dict]) -> None:
-    table = Table()
-    column_names = list(rows[0])
-    for name in column_names:
-        table.add_column(name)
-    for row in rows:
-        table.add_row(*(repr(row[name]) for name in column_names))
-    console = Console()
-    console.print(table)
+def print_as_records(dm_result: Iterable[Dict[str, str]]) -> None:
+    max_key_len = max(len(key) for key in dm_result[0].keys())
+    for record in dm_result:
+        for key, value in record.items():
+            print(f"{key.rjust(max_key_len)} : {str(value)}")
+        print(end="\n")
 
 
-def print_as_csv(dm_json):
-    with StringIO() as output:
-        writer = csv.DictWriter(output, fieldnames=dm_json[0].keys())
-        writer.writeheader()
-        writer.writerows(dm_json)
-        print(output.getvalue(), end="")
+def print_as_csv(dm_result: Iterable[Dict[str, str]]) -> None:
+    writer = csv.DictWriter(sys.stdout, fieldnames=dm_result[0].keys())
+    writer.writeheader()
+    writer.writerows(dm_result)
 
 
-def print_as_json(dm_json):
-    print(json.dumps(dm_json, indent=2))
+def print_as_json(dm_result: Iterable[Dict[str, str]]) -> None:
+    print(json.dumps(dm_result, indent=2))
 
 
-output_formats = {"json": print_as_json, "csv": print_as_csv, "table": print_as_table}
+OUTPUT_FORMATS = {"json": print_as_json, "csv": print_as_csv, "records": print_as_records}
 
 
-def main():
+def main() -> int:
     parser = argparse.ArgumentParser(
         description="Print CONTENTdm collection information",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -51,9 +44,9 @@ def main():
         "--output",
         type=str,
         action="store",
-        choices=list(output_formats.keys()),
-        default="table",
-        help=f"Output format",
+        choices=list(OUTPUT_FORMATS.keys()),
+        default="records",
+        help="Output format",
     )
     parser.add_argument(
         "--columns",
@@ -63,31 +56,28 @@ def main():
     )
     args = parser.parse_args()
 
-    try:
+    with requests.Session() as session:
         if args.alias:
             dm_result = cdm_api.get_collection_field_info(
-                args.repository_url, args.alias, requests
+                repo_url=args.repository_url,
+                collection_alias=args.alias,
+                session=session,
             )
         else:
-            dm_result = cdm_api.get_collection_list(args.repository_url, requests)
-    except cdm_api.DmError as err:
-        print(err, file=sys.stderr)
-        sys.exit(1)
+            dm_result = cdm_api.get_collection_list(
+                repo_url=args.repository_url, session=session
+            )
 
     if args.columns:
         columns = args.columns.split(",")
-        try:
-            dm_result = [
-                {column: entry[column] for column in columns} for entry in dm_result
-            ]
-        except KeyError as err:
-            print(f"{err.args[0]!r} is not a valid column name", file=sys.stderr)
-            sys.exit(1)
-    if args.output and args.output not in output_formats:
-        print(f"{args.output!r} is not a valid output format", file=sys.stderr)
-        sys.exit(1)
-    output_formats[args.output](dm_result)
+        dm_result = [
+            {column: entry[column] for column in columns} for entry in dm_result
+        ]
+
+    OUTPUT_FORMATS[args.output](dm_result)
+
+    return 0
 
 
 if __name__ == "__main__":
-    main()
+    raise SystemExit(main())
