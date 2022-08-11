f7b332f986794dce3c02b8eeccad7fd4a1b42508
Nick <versteeg@ohio.edu>
2022-08-11 17:04:21 -0400

    Rewrite ftpmdc2catcher using the new FtP API module

--- src/cdm_util_scripts/ftpmdc2catcher.py
+++ src/cdm_util_scripts/ftpmdc2catcher.py
@@ -2,31 +2,32 @@ import requests
 import tqdm
 
 import json
-import re
 
-from typing import List, Dict, Any
+from typing import List, Dict
 
-from cdm_util_scripts import ftp_api
+from cdm_util_scripts import ftp_api2 as ftp_api
 from cdm_util_scripts import cdm_api
 
 
 def config_ids_to_cdm_nicks(
-    config: List[Dict[str, Any]], field_mapping: Dict[str, List[str]]
+    config: ftp_api.FtpStructuredDataConfig, field_mapping: Dict[str, List[str]]
 ) -> Dict[str, List[str]]:
+    if not config.fields:
+        return {}
     labels_to_config_ids = {
-        field_config["label"]: field_config["@id"] for field_config in config
+        field_config.label: field_config.url for field_config in config.fields
     }
     return {labels_to_config_ids[name]: nicks for name, nicks in field_mapping.items()}
 
 
 def structured_data_to_catcher_edit(
-    dmrecord: str, data: List[Dict[str, Any]], ids_to_nicks: Dict[str, List[str]]
+    dmrecord: str, data: ftp_api.FtpStructuredData, ids_to_nicks: Dict[str, List[str]]
 ) -> Dict[str, str]:
     edit = {"dmrecord": dmrecord}
-    for field_data in data:
-        config_id = field_data["config"]
+    for field_data in data.data:
+        config_id = field_data.config
         nicks = ids_to_nicks[config_id]
-        value = field_data["value"]
+        value = field_data.value
         value = value if isinstance(value, str) else "; ".join(value)
         for nick in nicks:
             edit[nick] = value
@@ -42,70 +43,43 @@ def ftpmdc2catcher(
     field_mapping = cdm_api.read_csv_field_mapping(field_mapping_csv_path)
 
     with requests.Session() as session:
-        print("Locating collection manifest...")
-        manifest_url = ftp_api.get_collection_manifest_url(
+        print("Requesting project information...")
+        ftp_project = ftp_api.request_ftp_project(
+            base_url="https://fromthepage.com",
             slug=ftp_slug,
-            collection_name=ftp_project_name,
-            session=session,
+            project_label=ftp_project_name,
+            session=session
         )
 
-        print("Getting structured data configuration...")
-        work_configuration = ftp_api.get_project_structured_data_work_config(
-            manifest_url, session=session
-        )
-        page_configuration = ftp_api.get_project_structured_data_page_config(
-            manifest_url, session=session
-        )
-        if work_configuration is None and page_configuration is None:
+        print("Requesting structured data configuration...")
+        work_configuration = ftp_project.request_work_structured_data_config(session=session)
+        page_configuration = ftp_project.request_page_structured_data_config(session=session)
+        if not work_configuration.fields and not page_configuration.fields:
             raise ValueError("project has no Metadata Fields configuration")
 
-        work_config_ids_to_cdm_nicks = (
-            config_ids_to_cdm_nicks(work_configuration["config"], field_mapping)
-            if work_configuration
-            else None
-        )
-        page_config_ids_to_cdm_nicks = (
-            config_ids_to_cdm_nicks(page_configuration["config"], field_mapping)
-            if page_configuration
-            else None
-        )
+        work_config_ids_to_cdm_nicks = config_ids_to_cdm_nicks(work_configuration, field_mapping)
+        page_config_ids_to_cdm_nicks = config_ids_to_cdm_nicks(page_configuration, field_mapping)
 
-        print("Gathering project metadata...")
-        ftp_collection = ftp_api.get_ftp_collection(
-            manifest_url=manifest_url, session=session
-        )
+        if page_configuration.fields:
+            print("Requesting project works...")
+            ftp_project.request_works(session=session)
+
         edits = []
-        for ftp_work in tqdm.tqdm(ftp_collection.works):
+        for ftp_work in tqdm.tqdm(ftp_project.works):
             # Keep page-level edits before object-level edits to avoid locking CONTENTdm objects
-            if page_configuration:
-                manifest = ftp_api.get_ftp_manifest(
-                    ftp_work.ftp_manifest_url, session=session
-                )
-                for canvas in manifest["sequences"]["canvases"]:
-                    dmrecord = re.match(
-                        r".*/iiif/[^:]+:(\d+)/canvas/c\d+$", canvas["@id"]
-                    ).groups()[0]
-                    canvas_structured = [
-                        seeAlso
-                        for seeAlso in canvas["seeAlso"]
-                        if seeAlso["label"].startswith("Structured data")
-                    ][0]
-                    response = session.get(canvas_structured["@id"])
-                    response.raise_for_status()
-                    page_data = response.json()
+            if page_configuration.fields:
+                for ftp_page in ftp_work.pages:
                     edit = structured_data_to_catcher_edit(
-                        dmrecord, page_data["data"], page_config_ids_to_cdm_nicks
+                        dmrecord=ftp_page.cdm_page_dmrecord,
+                        data=ftp_page.request_structured_data(session=session),
+                        ids_to_nicks=page_config_ids_to_cdm_nicks,
                     )
                     edits.append(edit)
             if work_configuration:
-                work_structured_url = (
-                    ftp_work.ftp_manifest_url.rpartition("/")[0] + "/structured"
-                )
-                response = session.get(work_structured_url)
-                response.raise_for_status()
-                result = response.json()
                 edit = structured_data_to_catcher_edit(
-                    ftp_work.dmrecord, result["data"], work_config_ids_to_cdm_nicks
+                    dmrecord=ftp_work.cdm_object_dmrecord,
+                    data=ftp_work.request_structured_data(session=session),
+                    ids_to_nicks=work_config_ids_to_cdm_nicks
                 )
                 edits.append(edit)
 
