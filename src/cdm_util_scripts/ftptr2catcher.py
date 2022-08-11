8b54841fb60ef026a71ff482bae00d2b183782fb
Nick <versteeg@ohio.edu>
2022-08-11 18:19:01 -0400

    Rewrite ftptr2catcher using the new FtP API module

--- src/cdm_util_scripts/ftptr2catcher.py
+++ src/cdm_util_scripts/ftptr2catcher.py
@@ -1,136 +1,33 @@
 import requests
 import tqdm
 
-import re
 import json
-import argparse
-from typing import Dict, Any, Iterator, Tuple, Iterable, List, Optional
 
-from cdm_util_scripts import ftp_api
-
-
-def parse_canvas_id(url: str) -> Tuple[str, str]:
-    match = re.search(r"/iiif/([A-Za-z0-9\-]+)[:/](\d+)/canvas/c\d+$", url)
-    if match:
-        return match.groups()
-    raise ValueError(f"Couldn't parse URL {url!r}")
-
-
-def iter_manifest_sequence(
-    manifest: Dict[str, Any], transcript_type: str
-) -> Iterator[Tuple[str, str]]:
-    for canvas in manifest["sequences"][0]["canvases"]:
-        _, dmrecord = parse_canvas_id(canvas["@id"])
-        url = [
-            seeAlso["@id"]
-            for seeAlso in canvas["seeAlso"]
-            if seeAlso["label"] == transcript_type
-        ]
-        if not url:
-            raise ValueError(f"transcript type {transcript_type!r} not found")
-        yield dmrecord, url[0]
-
-
-def get_manifest_catcher_edits(
-    manifest: Dict[str, Any],
-    transcript_nick: str,
-    transcript_type: str,
-    session: requests.Session,
-) -> List[Dict[str, str]]:
-    catcher_edits = []
-    for dmrecord, url in tqdm.tqdm(
-        list(iter_manifest_sequence(manifest=manifest, transcript_type=transcript_type)),
-    ):
-        transcript_text = ftp_api.get_ftp_transcript(url=url, session=session)
-        catcher_edits.append(
-            {
-                "dmrecord": dmrecord,
-                transcript_nick: transcript_text.strip(),
-            }
-        )
-    return catcher_edits
-
-
-def get_manifests_catcher_edits(
-    manifest_urls: Iterable[str],
-    transcript_nick: str,
-    transcript_type: str,
-    session: requests.Session,
-) -> List[Dict[str, str]]:
-    catcher_edits = []
-    for manifest_url in manifest_urls:
-        print(f"Requesting {manifest_url!r}...")
-        manifest = ftp_api.get_ftp_manifest(manifest_url, session)
-        catcher_edits.extend(
-            get_manifest_catcher_edits(
-                manifest=manifest,
-                transcript_nick=transcript_nick,
-                transcript_type=transcript_type,
-                session=session,
-            )
-        )
-    return catcher_edits
+from cdm_util_scripts import ftp_api2 as ftp_api
 
 
 def ftptr2catcher(
-        manifests_listing_path: str,
-        transcript_nick: str,
-        output_file_path: str,
-        transcript_type: str,
+    manifests_listing_path: str,
+    transcript_nick: str,
+    output_file_path: str,
+    transcript_type: str,
 ) -> None:
     with open(manifests_listing_path, mode="r", encoding="utf-8") as fp:
         manifest_urls = [line.strip() for line in fp.readlines()]
 
     with requests.Session() as session:
-        catcher_edits = get_manifests_catcher_edits(
-            manifest_urls=manifest_urls,
-            transcript_nick=transcript_nick,
-            transcript_type=transcript_type,
-            session=session,
-        )
+        catcher_edits = []
+        for manifest_url in tqdm.tqdm(manifest_urls):
+            ftp_work = ftp_api.FtpWork.from_url(manifest_url, session=session)
+            for ftp_page in ftp_work.pages:
+                catcher_edits.append({
+                    "dmrecord": ftp_page.cdm_page_dmrecord,
+                    transcript_nick: ftp_page.request_transcript(
+                        label=transcript_type,
+                        session=session,
+                    )
+                })
 
     print("Writing JSON file...")
     with open(output_file_path, mode="w", encoding="utf-8") as fp:
         json.dump(catcher_edits, fp, indent=2)
-
-
-def main(test_args: Optional[Iterable[str]] = None) -> None:
-    parser = argparse.ArgumentParser(
-        description="Get transcripts from a list of FromThePage manuscripts in cdm-catcher JSON format",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    )
-    parser.add_argument(
-        "manifests_listing_path",
-        type=str,
-        help="Path to file listing FromThePage manifest links",
-    )
-    parser.add_argument(
-        "transcript_nick",
-        type=str,
-        help="CONTENTdm field nickname for the transcript field",
-    )
-    parser.add_argument(
-        "output_file",
-        type=str,
-        help="Path to write cdm-catcher JSON file",
-    )
-    parser.add_argument(
-        "--transcript_type",
-        type=str,
-        default="Verbatim Plaintext",
-        help="FromThePage transcript type",
-    )
-    args = parser.parse_args(test_args)
-
-    ftptr2catcher(
-        manifests_listing_path=args.manifests_listing_path,
-        transcript_nick=args.transcript_nick,
-        output_file_path=args.output_file,
-        transcript_type=args.transcript_type,
-    )
-
-    return 0
-
-
-if __name__ == "__main__":
-    raise SystemExit(main())
--- tests/test_ftptr2catcher.py
+++ tests/test_ftptr2catcher.py
@@ -8,63 +8,14 @@ from cdm_util_scripts import ftp_api
 
 
 SPECIMEN_MANIFEST_URL = "https://fromthepage.com/iiif/25013044/manifest"
-
-
-@pytest.fixture
-def ftp_manifest():
-    return ftp_api.get_ftp_manifest(
-        SPECIMEN_MANIFEST_URL, session=requests
-    )
-
-
-@pytest.mark.parametrize("url,alias,dmrecord", [
-    ("https://cdm15808.contentdm.oclc.org/iiif/mss:188/canvas/c1", "mss", "188"),
-    ("https://cdm15808.contentdm.oclc.org/digital/iiif/p15808coll19/1959/canvas/c0", "p15808coll19", "1959"),
-])
-def test_parse_canvas_id(url, alias, dmrecord):
-    assert ftptr2catcher.parse_canvas_id(url) == (alias, dmrecord)
-
-
-@pytest.mark.default_cassette("ftp_manifest.yaml")
-@pytest.mark.vcr
-def test_iter_manifest_sequence(ftp_manifest):
-    for dmrecord, url in ftptr2catcher.iter_manifest_sequence(
-        ftp_manifest, transcript_type="Verbatim Plaintext"
-    ):
-        assert int(dmrecord)
-        assert url.startswith("https://")
-
-
-@pytest.mark.default_cassette("ftp_manifest.yaml")
-@pytest.mark.vcr
-def test_get_manifest_catcher_edits(ftp_manifest):
-    transcript_nick = "transc"
-    catcher_edits = ftptr2catcher.get_manifest_catcher_edits(
-        ftp_manifest,
-        transcript_nick=transcript_nick,
-        transcript_type="Verbatim Plaintext",
-        session=requests,
-    )
-    for edit in catcher_edits:
-        assert int(edit["dmrecord"])
-        assert set(edit) == {"dmrecord", transcript_nick}
-
-
-@pytest.mark.vcr
-def test_get_manifests_catcher_edits():
-    transcript_nick = "transc"
-    transcript_type = "Verbatim Plaintext"
-    catcher_edits = ftptr2catcher.get_manifests_catcher_edits(
-        [
-            SPECIMEN_MANIFEST_URL,
-        ],
-        transcript_type=transcript_type,
-        transcript_nick=transcript_nick,
-        session=requests,
-    )
-    for edit in catcher_edits:
-        assert set(edit) == {"dmrecord", transcript_nick}
-        assert int(edit["dmrecord"])
+STARTS = [
+    ("8358", "LST 134"),
+    ("8359", "Ackley,"),
+    ("8360", "- for Cornelius Ryan 2 -"),
+    ("8361", "- for Cornelius Ryan 3 -"),
+    ("8362", "402- 2nd Ave"),
+    ("8363", "THE READER'S DIGEST"),
+]
 
 
 @pytest.mark.vcr
@@ -75,14 +26,17 @@ def test_ftptr2catcher(tmp_path):
     )
     output_path = tmp_path / "output.json"
     transcript_nick = "transc"
+
     ftptr2catcher.ftptr2catcher(
         manifests_listing_path=manifests_listing_path,
         transcript_nick=transcript_nick,
         output_file_path=output_path,
         transcript_type="Verbatim Plaintext",
     )
+
     with open(output_path, mode="r", encoding="utf-8") as fp:
         output_json = json.load(fp)
-    for edit in output_json:
+    for edit, (dmrecord, start) in zip(output_json, STARTS):
+        assert edit["dmrecord"] == dmrecord
+        assert edit[transcript_nick].startswith(start)
         assert set(edit) == {"dmrecord", transcript_nick}
-        assert int(edit["dmrecord"])
