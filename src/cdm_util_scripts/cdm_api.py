0b76b9c5533fa2c427f83d5de1ea1ea4b7243dcc
Nick <versteeg@ohio.edu>
2022-08-16 14:07:07 -0400

    Rewrite CONTENTdm API module

--- src/cdm_util_scripts/cdm_api.py
+++ src/cdm_util_scripts/cdm_api.py
@@ -2,82 +2,138 @@ from requests import Session
 
 import csv
 import collections
+import enum
 
-from typing import Dict, List, Union, Tuple, Sequence
+from typing import Dict, List, Union, Tuple, NamedTuple, Optional, Any
 
 
 class DmError(Exception):
     pass
 
 
-def get_dm(url: str, session: Session):
+def request_dm(url: str, session: Session):
     response = session.get(url)
     response.raise_for_status()
     dm_result = response.json()
-    if 'code' in dm_result and 'message' in dm_result:
-        raise DmError(dm_result['message'])
+    if "code" in dm_result and "message" in dm_result:
+        raise DmError(dm_result["message"])
     return dm_result
 
 
-def get_collection_field_info(repo_url: str, collection_alias: str, session: Session) -> dict:
-    url = '/'.join([
-        repo_url.rstrip('/'),
-        'digital/bl/dmwebservices/index.php?q=dmGetCollectionFieldInfo',
-        collection_alias,
-        'json'
-    ])
-    return get_dm(url, session)
+class CdmCollectionInfo(NamedTuple):
+    alias: str
+    name: str
+    path: str
+    secondary_alias: str
 
 
-def get_collection_list(repo_url: str, session: Session) -> list:
-    url = '/'.join([
-        repo_url.rstrip('/'),
-        'digital/bl/dmwebservices/index.php?q=dmGetCollectionList/json'
-    ])
-    return get_dm(url, session)
+def request_collection_list(
+    instance_url: str, session: Session
+) -> List[CdmCollectionInfo]:
+    instance_url = instance_url.rstrip("/")
+    url = "/".join(
+        [instance_url, "digital/bl/dmwebservices/index.php?q=dmGetCollectionList/json"]
+    )
+    return [CdmCollectionInfo(**info) for info in request_dm(url=url, session=session)]
 
 
-def get_cdm_item_info(
-        cdm_repo_url: str,
-        cdm_collection_alias: str,
-        dmrecord: str,
-        session: Session
-) -> Dict[str, str]:
-    cdm_repo_url = cdm_repo_url.rstrip('/')
-    url = f"{cdm_repo_url}/digital/bl/dmwebservices/index.php?q=dmGetItemInfo/{cdm_collection_alias}/{dmrecord}/json"
-    item_info = get_dm(url, session)
-    return {nick: value or '' for nick, value in item_info.items()}
+class CdmVocabType(enum.Enum):
+    builtin = enum.auto()
+    custom = enum.auto()
 
 
-def get_cdm_collection_field_vocab(
-        cdm_repo_url: str,
-        cdm_collection_alias: str,
-        cdm_field_nick: str,
-        session: Session
+class CdmVocabInfo(NamedTuple):
+    vocab_type: CdmVocabType
+    key: str
+
+
+class CdmFieldInfo(NamedTuple):
+    name: str
+    nick: str
+    type: str
+    size: int
+    find: str
+    req: int
+    search: int
+    hide: int
+    vocdb: str
+    vocab: int
+    dc: str
+    admin: int
+    readonly: int
+
+    def get_vocab_info(self) -> Optional[CdmVocabInfo]:
+        if not self.vocab:
+            return None
+        vocab_type = CdmVocabType.builtin if self.vocdb else CdmVocabType.custom
+        key = self.vocdb if self.vocdb else self.nick
+        return CdmVocabInfo(
+            vocab_type=vocab_type,
+            key=key,
+        )
+
+
+def request_field_infos(
+    instance_url: str, collection_alias: str, session: Session
+) -> List[CdmFieldInfo]:
+    instance_url = instance_url.rstrip("/")
+    url = "/".join(
+        [
+            instance_url,
+            "digital/bl/dmwebservices/index.php?q=dmGetCollectionFieldInfo",
+            collection_alias,
+            "json",
+        ]
+    )
+    return [CdmFieldInfo(**info) for info in request_dm(url=url, session=session)]
+
+
+CdmItemInfo = Dict[str, str]
+
+
+def request_item_info(
+    instance_url: str, collection_alias: str, dmrecord: str, session: Session
+) -> CdmItemInfo:
+    instance_url = instance_url.rstrip("/")
+    url = f"{instance_url}/digital/bl/dmwebservices/index.php?q=dmGetItemInfo/{collection_alias}/{dmrecord}/json"
+    item_info = request_dm(url=url, session=session)
+    return {nick: value or "" for nick, value in item_info.items()}
+
+
+CdmFieldVocab = List[str]
+
+
+def request_field_vocab(
+    instance_url: str, collection_alias: str, field_nick: str, session: Session
+) -> CdmFieldVocab:
+    instance_url = instance_url.rstrip("/")
+    url = f"{instance_url}/digital/bl/dmwebservices/index.php?q=dmGetCollectionFieldVocabulary/{collection_alias}/{field_nick}/0/1/json"
+    return request_dm(url=url, session=session)
+
+
+def request_page_pointers(
+    instance_url: str, collection_alias: str, dmrecord: str, session: Session
 ) -> List[str]:
-    cdm_repo_url = cdm_repo_url.rstrip('/')
-    url = f"{cdm_repo_url}/digital/bl/dmwebservices/index.php?q=dmGetCollectionFieldVocabulary/{cdm_collection_alias}/{cdm_field_nick}/0/1/json"
-    return get_dm(url, session)
-
-
-def get_cdm_page_pointers(repo_url: str, alias: str, dmrecord: str, session: Session) -> List[str]:
-    repo_url = repo_url.rstrip('/')
-    url = f"{repo_url}/digital/bl/dmwebservices/index.php?q=dmGetCompoundObjectInfo/{alias}/{dmrecord}/json"
-    cpd_object_info = get_dm(url, session)
+    instance_url = instance_url.rstrip("/")
+    url = f"{instance_url}/digital/bl/dmwebservices/index.php?q=dmGetCompoundObjectInfo/{collection_alias}/{dmrecord}/json"
+    cpd_object_info = request_dm(url=url, session=session)
     if cpd_object_info["type"] == "Monograph":
         _, page_pointers = _destructure_nodes(cpd_object_info["node"])
     else:
-        page_pointers = [page['pageptr'] for page in cpd_object_info['page']]
+        page_pointers = [page["pageptr"] for page in cpd_object_info["page"]]
     return page_pointers
 
 
 def _destructure_nodes(
-    node: Union[dict, list]
+    node: Union[Dict[str, Any], List[Any]]
 ) -> Tuple[List[Tuple[int, str]], List[str]]:
     pages_index = []
     page_pointers = []
 
-    def walk_nodes(node: Union[dict, list], depth: int = 0,) -> None:
+    def walk_nodes(
+        node: Union[Dict[str, Any], List[Any]],
+        depth: int = 0,
+    ) -> None:
         if "page" not in node:
             node_pages = []
         elif isinstance(node["page"], dict):
@@ -102,73 +158,58 @@ def _destructure_nodes(
     return pages_index, page_pointers
 
 
-def read_csv_field_mapping(filename: str) -> Dict[str, List[str]]:
-    with open(filename, mode='r', encoding='utf-8') as fp:
+CdmFieldMapping = Dict[str, List[str]]
+
+
+def read_csv_field_mapping(filename: str) -> CdmFieldMapping:
+    with open(filename, mode="r", encoding="utf-8") as fp:
         reader = csv.DictReader(fp)
-        if reader.fieldnames != ['name', 'nick']:
-            raise ValueError("column mapping CSV must have 'name' and 'nick' column titles in that order")
+        if reader.fieldnames != ["name", "nick"]:
+            raise ValueError(
+                "column mapping CSV must have 'name' and 'nick' column titles in that order"
+            )
         field_mapping = collections.defaultdict(list)
         for row in reader:
-            field_mapping[row['name']].append(row['nick'].strip())
+            field_mapping[row["name"]].append(row["nick"].strip())
     return dict(field_mapping)
 
 
-def apply_field_mapping(ftp_fields: Dict[str, str], field_mapping: Dict[str, Sequence[str]]) -> Dict[str, str]:
+def apply_field_mapping(
+    fields: Dict[str, str], field_mapping: CdmFieldMapping
+) -> Dict[str, str]:
     accumulator: Dict[str, str] = dict()
     for label, nicks in field_mapping.items():
-        ftp_field = ftp_fields[label]
+        field = fields[label]
         for nick in nicks:
             if nick in accumulator:
-                if ftp_field:
+                if field:
                     if accumulator[nick]:
-                        accumulator[nick] = '; '.join([
-                            accumulator[nick],
-                            ftp_field
-                        ])
+                        accumulator[nick] = "; ".join([accumulator[nick], field])
                     else:
-                        accumulator[nick] = ftp_field
+                        accumulator[nick] = field
             else:
-                accumulator[nick] = ftp_field
+                accumulator[nick] = field
     return accumulator
 
 
-def build_vocabs_index(cdm_fields_info: List[Dict[str, Union[str, int]]]) -> Dict[str, Dict[str, str]]:
-    vocabs_index = dict()
-    for field_info in cdm_fields_info:
-        if field_info['vocab']:
-            nick = field_info['nick']
-            vocdb = field_info['vocdb']
-            vocabs_index[nick] = {
-                'type': 'vocdb' if vocdb else 'vocab',
-                'name': vocdb if vocdb else nick,
-            }
-    return vocabs_index
-
-
-def get_vocabs(
-        cdm_repo_url: str,
-        cdm_collection_alias: str,
-        vocabs_index: Dict[str, Dict[str, str]],
-        session: Session,
-        verbose: bool = True
-) -> Dict[str, Dict[str, List[str]]]:
-    vocses = {
-        'vocab': dict(),
-        'vocdb': dict(),
-    }
-    for nick, index in vocabs_index.items():
-        vocs = vocses[index['type']]
-        if index['name'] in vocs:
+def request_vocabs(
+    instance_url: str,
+    collection_alias: str,
+    fields_info: List[CdmFieldInfo],
+    session: Session,
+) -> Dict[CdmVocabInfo, List[str]]:
+    vocabs = dict()
+    for field_info in fields_info:
+        vocab_info = field_info.get_vocab_info()
+        if vocab_info is None or vocab_info in vocabs:
             continue
-        if verbose:
-            print(f"Requesting {index['type']} for {nick!r}... ", end='')
-        vocab = get_cdm_collection_field_vocab(
-            cdm_repo_url=cdm_repo_url,
-            cdm_collection_alias=cdm_collection_alias,
-            cdm_field_nick=nick,
-            session=session
+        print(
+            f"Requesting {field_info.name if vocab_info.vocab_type is CdmVocabType.custom else vocab_info.key!r} vocab..."
         )
-        if verbose:
-            print(f"found {len(vocab)} terms.")
-        vocs[index['name']] = vocab
-    return vocses
+        vocabs[vocab_info] = request_field_vocab(
+            instance_url=instance_url,
+            collection_alias=collection_alias,
+            field_nick=field_info.nick,
+            session=session,
+        )
+    return vocabs
--- tests/test_cdm_api.py
+++ tests/test_cdm_api.py
@@ -5,97 +5,164 @@ from cdm_util_scripts import cdm_api
 
 
 @pytest.mark.vcr
-def test_get_cdm_item_info():
-    item_info = cdm_api.get_cdm_item_info(
-        cdm_repo_url='https://cdmdemo.contentdm.oclc.org',
-        cdm_collection_alias='oclcsample',
-        dmrecord='102',
-        session=requests,
-    )
-    assert item_info['dmrecord']
+def test_request_collection_list():
+    with requests.Session() as session:
+        collection_list = cdm_api.request_collection_list(
+            instance_url="https://cdmdemo.contentdm.oclc.org",
+            session=session,
+        )
+    for collection_info in collection_list:
+        assert isinstance(collection_info, cdm_api.CdmCollectionInfo)
+        assert collection_info.alias
+        assert collection_info.name
+
+
+@pytest.mark.parametrize(
+    "field_info, vocab_info",
+    [
+        (
+            cdm_api.CdmFieldInfo(
+                name="Subject",
+                nick="subjec",
+                type="TEXT",
+                size=0,
+                find="a5",
+                req=0,
+                search=1,
+                hide=0,
+                vocdb="LCTGM",
+                vocab=1,
+                dc="subjec",
+                admin=0,
+                readonly=0,
+            ),
+            cdm_api.CdmVocabInfo(vocab_type=cdm_api.CdmVocabType.builtin, key="LCTGM"),
+        ),
+        (
+            cdm_api.CdmFieldInfo(
+                name="Subject",
+                nick="subjec",
+                type="TEXT",
+                size=0,
+                find="a5",
+                req=0,
+                search=1,
+                hide=0,
+                vocdb="",
+                vocab=1,
+                dc="subjec",
+                admin=0,
+                readonly=0,
+            ),
+            cdm_api.CdmVocabInfo(vocab_type=cdm_api.CdmVocabType.custom, key="subjec"),
+        ),
+        (
+            cdm_api.CdmFieldInfo(
+                name="Title",
+                nick="title",
+                type="TEXT",
+                size=0,
+                find="a0",
+                req=1,
+                search=1,
+                hide=0,
+                vocdb="",
+                vocab=0,
+                dc="title",
+                admin=0,
+                readonly=0,
+            ),
+            None,
+        ),
+    ],
+)
+def test_CdmFieldInfo_get_vocab_info(field_info, vocab_info):
+    assert field_info.get_vocab_info() == vocab_info
+
+
+@pytest.mark.default_cassette("field_infos.yaml")
+@pytest.mark.vcr
+def test_request_field_infos():
+    with requests.Session() as session:
+        field_infos = cdm_api.request_field_infos(
+            instance_url="https://cdmdemo.contentdm.oclc.org",
+            collection_alias="oclcsample",
+            session=session
+        )
+    for field_info in field_infos:
+        assert isinstance(field_info, cdm_api.CdmFieldInfo)
+        assert field_info.name
+        assert field_info.nick
+
+
+@pytest.mark.vcr
+def test_request_item_info():
+    with requests.Session() as session:
+        item_info = cdm_api.request_item_info(
+            instance_url="https://cdmdemo.contentdm.oclc.org",
+            collection_alias="oclcsample",
+            dmrecord="102",
+            session=session,
+        )
+    assert item_info["dmrecord"]
     for key, value in item_info.items():
         assert isinstance(value, str)
 
     with pytest.raises(cdm_api.DmError):
-        cdm_api.get_cdm_item_info(
-            cdm_repo_url='https://cdmdemo.contentdm.oclc.org',
-            cdm_collection_alias='oclcsample',
-            dmrecord='999',
-            session=requests,
+        with requests.Session() as session:
+            cdm_api.request_item_info(
+                instance_url="https://cdmdemo.contentdm.oclc.org",
+                collection_alias="oclcsample",
+                dmrecord="999",
+                session=session,
+            )
+
+
+@pytest.mark.vcr
+def test_request_field_vocab():
+    with requests.Session() as session:
+        vocab = cdm_api.request_field_vocab(
+            instance_url="https://cdmdemo.contentdm.oclc.org",
+            collection_alias="oclcsample",
+            field_nick="subjec",
+            session=session,
         )
-
-
-@pytest.mark.vcr
-def test_get_cdm_page_pointers():
-    session = requests.Session()
-    pointers = cdm_api.get_cdm_page_pointers(
-        repo_url='https://media.library.ohio.edu',
-        alias='p15808coll15',
-        dmrecord='4613',
-        session=requests,
-    )
-    assert pointers
-
-
-@pytest.mark.vcr
-def test_get_cdm_collection_field_vocab():
-    session = requests.Session()
-    vocab = cdm_api.get_cdm_collection_field_vocab(
-        cdm_repo_url='https://cdmdemo.contentdm.oclc.org',
-        cdm_collection_alias='oclcsample',
-        cdm_field_nick='subjec',
-        session=requests,
-    )
     assert vocab
 
 
-@pytest.mark.parametrize('field_mapping, result', [
-    ({'label': ['nick']}, {'nick': 'value'}),
-    ({'label': ['nick1', 'nick2']}, {'nick1': 'value', 'nick2': 'value'}),
-    ({'blank': ['nick'], 'label': ['nick']}, {'nick': 'value'}),
-    ({'label': ['nick'], 'blank': ['nick']}, {'nick': 'value'}),
-    ({'blank': ['nick', 'nick']}, {'nick': ''}),
-    ({'label': ['nick', 'nick']}, {'nick': 'value; value'}),
-    ({'label': ['nick'], 'label2': ['nick']}, {'nick': 'value; value2'}),
-    ({'label2': ['nick'], 'label': ['nick']}, {'nick': 'value2; value'}),
-])
-def test_apply_mapping(field_mapping, result):
-    ftp_fields = {
-        'label': 'value',
-        'label2': 'value2',
-        'blank': ''
-    }
-    mapped = cdm_api.apply_field_mapping(ftp_fields, field_mapping)
-    assert mapped == result
-
-
-@pytest.fixture
-def collection_field_info():
+@pytest.mark.vcr
+def test_request_page_pointers():
     with requests.Session() as session:
-        return cdm_api.get_collection_field_info(
-            repo_url='https://cdmdemo.contentdm.oclc.org',
-            collection_alias='oclcsample',
+        pointers = cdm_api.request_page_pointers(
+            instance_url="https://cdmdemo.contentdm.oclc.org",
+            collection_alias="oclcsample",
+            dmrecord="93",
             session=session,
         )
+        monograph_pointers = cdm_api.request_page_pointers(
+            instance_url="https://cdmdemo.contentdm.oclc.org",
+            collection_alias="oclcsample",
+            dmrecord="102",
+            session=session,
+        )
+    assert pointers
+    assert monograph_pointers
 
 
-@pytest.mark.default_cassette("collection_field_info.yaml")
-@pytest.mark.vcr
-def test_build_vocabs_index(collection_field_info):
-    vocabs_index = cdm_api.build_vocabs_index(collection_field_info)
-    assert vocabs_index == {'subjec': {'type': 'vocdb', 'name': 'LCTGM'}}
-
-
-@pytest.mark.default_cassette("collection_field_info.yaml")
-@pytest.mark.vcr
-def test_get_vocabs(collection_field_info):
-    vocabs_index = cdm_api.build_vocabs_index(collection_field_info)
-    vocabs = cdm_api.get_vocabs(
-        cdm_repo_url='https://cdmdemo.contentdm.oclc.org',
-        cdm_collection_alias='oclcsample',
-        vocabs_index=vocabs_index,
-        session=requests,
-    )
-    assert len(vocabs_index) == (len(vocabs['vocab']) + len(vocabs['vocdb']))
-    for nick, index in vocabs_index.items():
-        assert index['name'] in vocabs[index['type']]
+@pytest.mark.parametrize(
+    "field_mapping, result",
+    [
+        ({"label": ["nick"]}, {"nick": "value"}),
+        ({"label": ["nick1", "nick2"]}, {"nick1": "value", "nick2": "value"}),
+        ({"blank": ["nick"], "label": ["nick"]}, {"nick": "value"}),
+        ({"label": ["nick"], "blank": ["nick"]}, {"nick": "value"}),
+        ({"blank": ["nick", "nick"]}, {"nick": ""}),
+        ({"label": ["nick", "nick"]}, {"nick": "value; value"}),
+        ({"label": ["nick"], "label2": ["nick"]}, {"nick": "value; value2"}),
+        ({"label2": ["nick"], "label": ["nick"]}, {"nick": "value2; value"}),
+    ],
+)
+def test_apply_mapping(field_mapping, result):
+    ftp_fields = {"label": "value", "label2": "value2", "blank": ""}
+    mapped = cdm_api.apply_field_mapping(ftp_fields, field_mapping)
+    assert mapped == result
