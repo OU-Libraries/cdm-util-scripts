e3c01686a003a796d153cd9b7421d3554062ca11
Nick <versteeg@ohio.edu>
2022-08-23 17:30:40 -0400

    Revise catcherdiff to include DC identifier in report

--- src/cdm_util_scripts/catcherdiff.py
+++ src/cdm_util_scripts/catcherdiff.py
@@ -1,5 +1,6 @@
-from requests import Session
+import requests
 import jinja2
+import tqdm
 
 import json
 from datetime import datetime
@@ -7,82 +8,40 @@ from pathlib import Path
 
 from cdm_util_scripts import cdm_api
 
-from typing import Dict, List, Tuple, Any
+from typing import Dict, List, Any, NamedTuple, Iterable, Optional
 
 
-def get_cdm_items_info(
-        cdm_repo_url: str,
-        cdm_collection_alias: str,
-        cdm_catcher_edits: List[Dict[str, str]],
-        session: Session,
-        verbose: bool = True
-) -> List[Dict[str, str]]:
-    cdm_items_info = []
-    for n, edit in enumerate(cdm_catcher_edits, start=1):
-        if verbose:
-            print(f"Requesting CONTENTdm item info {n}/{len(cdm_catcher_edits)}...", end='\r')
-        cdm_items_info.append(
-            cdm_api.request_item_info(
-                instance_url=cdm_repo_url,
-                collection_alias=cdm_collection_alias,
-                dmrecord=edit['dmrecord'],
-                session=session
-            )
-        )
-    if verbose:
-        print(end='\n')
-    return cdm_items_info
-
-
-def collate_deltas(
-        cdm_catcher_edits: List[Dict[str, str]],
-        cdm_items_info: List[Dict[str, str]]
-) -> List[Tuple[dict, dict]]:
-    return [
-        (edit, {nick: item_info[nick] for nick in edit.keys()})
-        for edit, item_info in zip(cdm_catcher_edits, cdm_items_info)
-    ]
-
-
-def count_changes(deltas: List[Tuple[dict, dict]]) -> int:
-    edits_with_changes_count = 0
-    for delta in deltas:
-        for nick, value in delta[0].items():
-            if value != delta[1][nick]:
-                edits_with_changes_count += 1
-                break
-    return edits_with_changes_count
-
-
-def report_to_html(report: Dict[str, Any]) -> str:
-    env = jinja2.Environment(loader=jinja2.PackageLoader(__package__))
-    return env.get_template('catcherdiff-report.html.j2').render(report)
+class Delta(NamedTuple):
+    edit: Dict[str, str]
+    item_info: cdm_api.CdmItemInfo
 
 
 def catcherdiff(
-        cdm_instance_url: str,
-        cdm_collection_alias: str,
-        catcher_json_file_path: str,
-        report_file_path: str,
-        check_vocabs: bool,
+    cdm_instance_url: str,
+    cdm_collection_alias: str,
+    catcher_json_file_path: str,
+    report_file_path: str,
+    check_vocabs: bool,
 ) -> None:
-    with open(catcher_json_file_path, mode='r', encoding="utf-8") as fp:
-        cdm_catcher_edits = json.load(fp)
+    with open(catcher_json_file_path, mode="r", encoding="utf-8") as fp:
+        catcher_edits = json.load(fp)
 
-    with Session() as session:
+    with requests.Session() as session:
         print("Requesting CONTENTdm field info...")
         cdm_field_infos = cdm_api.request_field_infos(
             instance_url=cdm_instance_url,
             collection_alias=cdm_collection_alias,
-            session=session
+            session=session,
         )
-        cdm_items_info = get_cdm_items_info(
-            cdm_repo_url=cdm_instance_url,
-            cdm_collection_alias=cdm_collection_alias,
-            cdm_catcher_edits=cdm_catcher_edits,
-            session=session
+        print("Requesting CONTENTdm item info...")
+        deltas = request_deltas(
+            catcher_edits=catcher_edits,
+            instance_url=cdm_instance_url,
+            collection_alias=cdm_collection_alias,
+            session=session,
         )
         if check_vocabs:
+            print("Requesting CONTENTdm controlled vocabularies...")
             cdm_vocabs = cdm_api.request_vocabs(
                 instance_url=cdm_instance_url,
                 collection_alias=cdm_collection_alias,
@@ -92,29 +51,80 @@ def catcherdiff(
         else:
             cdm_vocabs = None
 
-    deltas = collate_deltas(cdm_catcher_edits, cdm_items_info)
     edits_with_changes_count = count_changes(deltas)
+    vocabs_by_nick = {
+        field_info.nick: cdm_vocabs[field_info.get_vocab_info()] if cdm_vocabs else None
+        for field_info in cdm_field_infos
+        if field_info.vocab
+    }
+    identifier_field_info = find_dc_field(cdm_field_infos, "Identifier")
+    identifier_nick = identifier_field_info.nick if identifier_field_info else None
+    title_field_info = find_dc_field(cdm_field_infos, "Title")
+    title_nick = title_field_info.nick if title_field_info else None
 
-    print(f"catcherdiff found {edits_with_changes_count} out of {len(deltas)} total edit actions would change at least one field.")
+    print(
+        f"catcherdiff found {edits_with_changes_count} out of {len(catcher_edits)} total edit actions would change at least one field."
+    )
 
     report = {
-        'cdm_repo_url': cdm_instance_url.rstrip('/'),
-        'cdm_collection_alias': cdm_collection_alias,
-        'cdm_field_infos': cdm_field_infos,
-        'vocabs_by_nick': {
-            field_info.nick: cdm_vocabs[field_info.get_vocab_info()] if cdm_vocabs else None
-            for field_info in cdm_field_infos if field_info.vocab
-        },
-        'catcher_json_file': Path(catcher_json_file_path).name,
-        'report_file': report_file_path,
-        'report_datetime': datetime.now().isoformat(),
-        'edits_with_changes_count': edits_with_changes_count,
-        'deltas': deltas,
-        'cdm_nick_to_name': {
+        "cdm_repo_url": cdm_instance_url.rstrip("/"),
+        "cdm_collection_alias": cdm_collection_alias,
+        "cdm_field_infos": cdm_field_infos,
+        "vocabs_by_nick": vocabs_by_nick,
+        "catcher_json_file": Path(catcher_json_file_path).name,
+        "report_file": report_file_path,
+        "report_datetime": datetime.now().isoformat(),
+        "edits_with_changes_count": edits_with_changes_count,
+        "deltas": deltas,
+        "identifier_nick": identifier_nick,
+        "title_nick": title_nick,
+        "cdm_nick_to_name": {
             field_info.nick: field_info.name for field_info in cdm_field_infos
         },
     }
 
     report_html = report_to_html(report)
-    with open(report_file_path, mode='w', encoding='utf-8') as fp:
+    with open(report_file_path, mode="w", encoding="utf-8") as fp:
         fp.write(report_html)
+
+
+def request_deltas(
+    catcher_edits: List[Dict[str, str]],
+    instance_url: str,
+    collection_alias: str,
+    session: requests.Session,
+) -> List[Delta]:
+    deltas: List[Delta] = []
+    for edit in tqdm.tqdm(catcher_edits):
+        item_info = cdm_api.request_item_info(
+            instance_url=instance_url,
+            collection_alias=collection_alias,
+            dmrecord=edit["dmrecord"],
+            session=session,
+        )
+        deltas.append(Delta(edit=edit, item_info=item_info))
+    return deltas
+
+
+def count_changes(deltas: List[Delta]) -> int:
+    edits_with_changes_count = 0
+    for delta in deltas:
+        for nick, value in delta.edit.items():
+            if value != delta.item_info[nick]:
+                edits_with_changes_count += 1
+                break
+    return edits_with_changes_count
+
+
+def find_dc_field(
+    cdm_field_infos: Iterable[cdm_api.CdmFieldInfo], dc_name: str
+) -> Optional[cdm_api.CdmFieldInfo]:
+    try:
+        return [info for info in cdm_field_infos if info.dc == dc_name][0]
+    except IndexError:
+        return None
+
+
+def report_to_html(report: Dict[str, Any]) -> str:
+    env = jinja2.Environment(loader=jinja2.PackageLoader(__package__))
+    return env.get_template("catcherdiff-report.html.j2").render(report)
--- tests/test_catcherdiff.py
+++ tests/test_catcherdiff.py
@@ -1,87 +1,15 @@
 import pytest
-import requests
 
 import json
 
 from cdm_util_scripts import catcherdiff
-from cdm_util_scripts import cdm_api
-
-
-@pytest.mark.parametrize('cdm_catcher_edits, cdm_items_info, result', [
-    (
-        [{'dmrecord': '1', 'nick': 'value1'}],
-        [{'dmrecord': '1', 'nick': 'value2', 'extra': 'extra'}],
-        [({'dmrecord': '1', 'nick': 'value1'}, {'dmrecord': '1', 'nick': 'value2'})]
-    )
-])
-def test_collate_deltas(cdm_catcher_edits, cdm_items_info, result):
-    deltas = catcherdiff.collate_deltas(
-        cdm_catcher_edits=cdm_catcher_edits,
-        cdm_items_info=cdm_items_info
-    )
-    assert deltas == result
-
-
-@pytest.mark.parametrize('cdm_catcher_edits, cdm_items_info', [
-    (
-        [{'dmrecord': '1', 'nick': 'value1', 'wrong': 'wrong1'}],
-        [{'dmrecord': '1', 'nick': 'value2', 'extra': 'extra'}]
-    )
-])
-def test_collate_deltas_raises(cdm_catcher_edits, cdm_items_info):
-    with pytest.raises(KeyError):
-        catcherdiff.collate_deltas(
-            cdm_catcher_edits=cdm_catcher_edits,
-            cdm_items_info=cdm_items_info
-        )
 
 
 @pytest.mark.vcr
-def test_report_to_html():
-    repo_url = 'https://cdmdemo.contentdm.oclc.org'
-    collection_alias = 'oclcsample'
-    with requests.Session() as session:
-        field_infos = cdm_api.request_field_infos(
-            instance_url=repo_url,
-            collection_alias=collection_alias,
-            session=session,
-        )
-    report_base = {
-        'cdm_instance_url': repo_url,
-        'cdm_collection_alias': collection_alias,
-        'cdm_field_infos': field_infos,
-        'catcher_json_file': 'catcher-edits.json',
-        'report_file': 'catcherdiff-report.html',
-        'report_datetime': '2021-01-01T00:00:00.000000',
-        'deltas': [
-            (
-                {'dmrecord': '1', 'subjec': 'value1'},
-                {'dmrecord': '1', 'subjec': 'value2'}
-            )
-        ],
-        'cdm_nick_to_name': {
-            field_info.nick: field_info.name for field_info in field_infos
-        },
-    }
-
-    assert catcherdiff.report_to_html({
-        **report_base,
-        'vocabs_by_nick': {"subjec": None},
-    })
-
-    assert catcherdiff.report_to_html({
-        **report_base,
-        'values_by_nick': {'subjec': ['value1']},
-    })
-
-
-@pytest.mark.vcr
-def test_catcherdiff(tmpdir):
-    catcher_edits = [
-        {"dmrecord": "71", "format": "PDF"}
-    ]
-    catcher_json_file_path = tmpdir / "catcher-edits.json"
-    report_file_path = tmpdir / "report.html"
+def test_catcherdiff(tmp_path):
+    catcher_edits = [{"dmrecord": "71", "format": "PDF"}]
+    catcher_json_file_path = tmp_path / "catcher-edits.json"
+    report_file_path = tmp_path / "report.html"
     with open(catcher_json_file_path, mode="w", encoding="utf-8") as fp:
         json.dump(catcher_edits, fp)
 
