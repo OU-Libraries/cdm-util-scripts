cacd964133d15ed4204d60acb387ce741cdbf7ac
Nick Ver Steegh <47987297+npversteegh@users.noreply.github.com>
2023-06-26 18:54:26 -0400

    Add catchertidy.py module and tests

---
+++ src/cdm_util_scripts/catchertidy.py
@@ -0,0 +1,79 @@
+import json
+import re
+
+from typing import Optional, List, Dict
+
+
+def catchertidy(
+    catcher_json_file_path: str,
+    output_file_path: str,
+    normalize_whitespace: bool,
+    replace_smart_chars: bool,
+    normalize_lcsh: List[str],
+    sort_terms: List[str],
+) -> None:
+    """Tidy up a cdm-catcher JSON edit."""
+    with open(catcher_json_file_path, mode="r", encoding="utf-8") as fp:
+        catcher_edits: List[Dict[str, str]] = json.load(fp)
+
+    tidy_edits: List[Dict[str, str]] = []
+    for edit in catcher_edits:
+        tidy_edit: Dict[str, str] = {"dmrecord": edit["dmrecord"]}
+        for nick, edit_value in edit.items():
+            if nick == "dmrecord":
+                continue
+
+            if normalize_whitespace:
+                edit_value = normalize_whitespace_(edit_value)
+
+            if replace_smart_chars:
+                edit_value = replace_smart_chars_(edit_value)
+
+            if nick in normalize_lcsh:
+                edit_value = normalize_lcsh_(edit_value)
+
+            if nick in sort_terms:
+                edit_value = sort_terms_(edit_value)
+
+            tidy_edit[nick] = edit_value
+        tidy_edits.append(tidy_edit)
+
+    with open(output_file_path, mode="w", encoding="utf-8") as fp:
+        json.dump(tidy_edits, fp)
+
+
+def normalize_whitespace_(value: str) -> str:
+    return " ".join(value.split())
+
+
+def replace_smart_chars_(value: str) -> str:
+    return value.translate(
+        {
+            0x201C: '"',
+            0x201D: '"',
+            0x2018: "'",
+            0x2019: "'",
+            0x2013: "-",
+            0x2014: "--",
+        }
+    )
+
+
+def normalize_lcsh_(terms: str) -> str:
+    normalized_terms: List[str] = []
+    for term in split_controlled_vocab(terms):
+        term = replace_smart_chars_(normalize_whitespace_(term))
+        parts = [part.strip() for part in term.rsplit("--")]
+        normalized_terms.append(" -- ".join(parts))
+    return "; ".join(normalized_terms)
+
+
+def sort_terms_(terms: str) -> str:
+    return sorted(split_controlled_vocab(terms))
+
+
+VOCAB_SPLIT_PAT = re.compile(r";\s*")
+
+
+def split_controlled_vocab(values: str) -> List[str]:
+    return [term for term in VOCAB_SPLIT_PAT.split(values) if term]
---
+++ tests/test_catchertidy.py
@@ -0,0 +1,81 @@
+import json
+
+import pytest
+
+from cdm_util_scripts import catchertidy
+
+
+def test_catchertidy(tmp_path):
+    edits = [
+        {
+            "dmrecord": "1",
+            "nicka": "The  quick \n brown fox  jumps\tover the “lazy” dog.",
+            "nickb": "World War, 1939-1945--Journalists;\tFrancis Pope, 1936---Biography; ",
+        },
+    ]
+    edits_path = tmp_path / "edits.json"
+    output_path = tmp_path / "output.json"
+    edits_path.write_text(json.dumps(edits))
+    catchertidy.catchertidy(
+        catcher_json_file_path=edits_path,
+        output_file_path=output_path,
+        normalize_whitespace=True,
+        replace_smart_chars=True,
+        normalize_lcsh=["nickb"],
+        sort_terms=["nickb"],
+    )
+    json.load(output_path.open()) == [
+        {
+            "dmrecord": "1",
+            "nicka": 'The quick brown fox jumps over the "lazy" dog.',
+            "nickb": "Francis Pope, 1936- -- Biography; World War, 1939-1945 -- Journalists",
+        }
+    ]
+
+
+@pytest.mark.parametrize(
+    "before,after",
+    [
+        ("", ""),
+        ("No change.", "No change."),
+        ("“double quotes”", '"double quotes"'),
+        ("‘single quotes’", "'single quotes'"),
+        ("em—dash", "em--dash"),
+        ("en–dash", "en-dash"),
+    ],
+)
+def test_replace_smart_chars_(before, after):
+    assert catchertidy.replace_smart_chars_(before) == after
+
+
+@pytest.mark.parametrize(
+    "before,after",
+    [
+        ("World War, 1939-1945--Journalists", "World War, 1939-1945 -- Journalists"),
+        (
+            "Soldiers--United States--20th century",
+            "Soldiers -- United States -- 20th century",
+        ),
+        ("Francis Pope, 1936---Biography", "Francis Pope, 1936- -- Biography"),
+        (
+            "World War, 1939-1945--Journalists; Soldiers -- United States -- 20th century",
+            "World War, 1939-1945 -- Journalists; Soldiers -- United States -- 20th century",
+        ),
+    ],
+)
+def test_normalize_lcsh_(before, after):
+    assert catchertidy.normalize_lcsh_(before) == after
+
+
+@pytest.mark.parametrize(
+    "before,after",
+    [
+        ("single term", ["single term"]),
+        ("term one; term two", ["term one", "term two"]),
+        ("term one;term two", ["term one", "term two"]),
+        ("term one;   term two", ["term one", "term two"]),
+        ("term one;\tterm two", ["term one", "term two"]),
+    ],
+)
+def test_split_controlled_vocab(before, after):
+    assert catchertidy.split_controlled_vocab(before) == after
